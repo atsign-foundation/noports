4fc98642d3e23368335c5f056a8e0e67b8fddac7
Colin Constable <colin@constable.cc>
2022-07-31 20:11:50 -0700

    Update sshnpd.dart
    SHOUT for sessions starting and ending

--- bin/sshnpd.dart
+++ bin/sshnpd.dart
@@ -234,7 +234,7 @@ void sshCallback(
     var hostname = sshList[3];
     _logger
         .info('ssh session started for $username to $hostname on port $port using localhost:$localPort on $hostname ');
-    _logger.warning('ssh session started from: ' + notification.from.toString() + " session: $sessionId");
+    _logger.shout('ssh session started from: ' + notification.from.toString() + " session: $sessionId");
 
     // var result = await Process.run('ssh', sshList);
 
@@ -268,7 +268,7 @@ void sshCallback(
           await client.done;
           stop = true;
           timer.cancel();
-          _logger.warning('ssh session complete for: ' + notification.from.toString() + " session: $sessionId");
+          _logger.shout('ssh session complete for: ' + notification.from.toString() + " session: $sessionId");
         }
       });
       // Answer ssh requests until none are left open
