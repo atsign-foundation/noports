79c106afaf4bf7afbc62a186cb0eb4a3ec2e06c9
gkc <gary.casey@gmail.com>
2023-03-18 08:52:21 +0000

    debug: in the event of an unhandled exception, having printed the exception and stack trace to stderr, then before exiting call `await stderr.flush()` with a timeout of 100 milliseconds

--- bin/sshnpd.dart
+++ bin/sshnpd.dart
@@ -23,8 +23,9 @@ void main(List<String> args) async {
   await runZonedGuarded<Future<void>>(() async {
     _main(args);
   }, (error, stackTrace) async {
-    stderr.writeln('sshnpd encountered exception: ${error.toString()}');
+    stderr.writeln('sshnpd: ${error.toString()}');
     stderr.writeln('stack trace: ${stackTrace.toString()}');
+    await stderr.flush().timeout(Duration(milliseconds: 100));
     exit(1);
   });
 }
