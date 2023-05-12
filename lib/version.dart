ee25ee4fdb4ba129739654d8503e45ba48bfa662
Colin Constable <colin@constable.cc>
2023-05-12 11:40:03 -0700

    chore: updated version number

--- lib/version.dart
+++ lib/version.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
-
-// Get the home directory or null if unknown.
+//Print version number
 void version() {
-  final String version = "3.0.0";
+  final String version = "3.2.0";
   stdout.writeln('Version : $version');
 }
