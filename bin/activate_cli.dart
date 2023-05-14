e2e2c4053747a60321b6fa697e055911a0d3cf09
Colin Constable <colin@constable.cc>
2023-05-13 17:32:52 -0700

    fix: nicer errors on at_activate, removed the nasty Dart errors with try{}

--- bin/activate_cli.dart
+++ bin/activate_cli.dart
@@ -2,5 +2,9 @@ import 'package:at_onboarding_cli/src/activate_cli/activate_cli.dart'
     as activate_cli;
 
 Future<void> main(List<String> args) async {
-  await activate_cli.main(args);
+  try {
+    await activate_cli.main(args);
+  } catch (e) {
+    print(e.toString());
+  }
 }
