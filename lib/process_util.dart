7349d50f755f2625fbc12bd1b2eb482e4d6073bc
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-24 18:18:49 -0400

    feat: `runCommand` workingDirectory optional arg

--- lib/process_util.dart
+++ lib/process_util.dart
@@ -1,10 +1,9 @@
-
 import 'dart:io';
 
 // Usage example: runCommand('ls -l', runInShell: true);
-Future<ProcessResult> runCommand(String command, {runInShell = false}) {
+Future<ProcessResult> runCommand(String command, {bool runInShell = false, String? workingDirectory}) {
   final List<String> split = command.split(' ');
   final String exec = split.first;
   final List<String> args = split.sublist(1);
-  return Process.run(exec, args, runInShell: runInShell);
+  return Process.run(exec, args, runInShell: runInShell, workingDirectory: workingDirectory);
 }
