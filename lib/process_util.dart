f56c7084aa760931fc32bfabacb8b6f2e73c5c4d
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-24 19:12:53 -0400

    feat: handle error in `runCommand`

--- lib/process_util.dart
+++ lib/process_util.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 // Usage example: runCommand('ls -l', runInShell: true);
-Future<ProcessResult> runCommand(String command, {bool runInShell = false, String? workingDirectory}) {
+Future<ProcessResult> runCommand(String command, {bool runInShell = false, String? workingDirectory}) async {
   final List<String> split = command.split(' ');
   final String exec = split.first;
   final List<String> args = split.sublist(1);
-  return Process.run(exec, args, runInShell: runInShell, workingDirectory: workingDirectory);
+  final ProcessResult pres = await Process.run(exec, args, runInShell: runInShell, workingDirectory: workingDirectory);
+  if(pres.exitCode != 0) {
+    throw Exception('runCommand: $command | pres.exitCode: ${pres.exitCode}');
+  }
+  return pres;
 }
