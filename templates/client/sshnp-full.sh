3e34951f857a04289ec5b55f71e076a544391475
xavierchanth <xchanthavong@gmail.com>
2023-06-02 01:17:52 -0400

    chore: add client templates

---
+++ templates/client/sshnp-full.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+BINARY="/usr/local/bin/sshnp";
+FROM="$CLIENT_ATSIGN";
+TO="$DEVICE_MANAGER_ATSIGN";
+HOST="$DEFAULT_HOST_ATSIGN";
+
+usage() {
+  echo "sshnp$DEVICE_MANAGER_ATSIGN [-h <host>] <device name>";
+}
+
+parse_args() {
+  while [ $# -gt 0 ]; do
+    case "$1" in
+    -h|--host)
+      if [ $# -lt 0 ]; then
+        echo "Missing argument for $1";
+        exit 1;
+      fi
+      HOST="$2"
+      shift 2
+      ;;
+    *)
+      DEVICE="$1"
+      shift
+      ;;
+    esac
+    shift
+  done
+  if [ -z "$DEVICE" ]; then
+    echo "No device specified";
+    usage;
+    exit 1;
+  fi
+}
+
+# -f = client atSign ("from")
+# -t = device manager atSign ("to")
+# -h = host rendezvous server atSign (SRS)
+# -d = device name
+eval "$BINARY -f $FROM -t $TO -h $HOST -d $DEVICE";
