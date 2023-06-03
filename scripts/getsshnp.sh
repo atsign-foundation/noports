3a009ea972884389bae94216a98ad5a5d2ed9472
xavierchanth <xchanthavong@gmail.com>
2023-06-02 21:05:17 -0400

    feat: Finish getsshnpsh

--- scripts/getsshnp.sh
+++ scripts/getsshnp.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
-SSHNP_DEV_MODE=false;
 BINARY_NAME="sshnp";
 
 norm_atsign() {
   KEY="$1" # Get the variable name
   INPUT=${!KEY} # Get the value of the variable
-  # Add @ if missing
   shopt -s extglob
   ATSIGN=${INPUT/#?(\@)/\@} # Add @ if missing
   shopt -u extglob
   export "${KEY}"="$ATSIGN" # Set the variable to new value
 }
 
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -f, --from <address>    (mandatory)  Client address"
+  echo "  -t, --to <address>      (mandatory)  Device manager address"
+  echo "  -h, --host <region>     (mandatory)  Default host rendezvous region code (am, eu, ap)"
+  echo "  -l, --local <path>                   Install using local zip/tgz"
+  echo "  -d, --dev <path>                     Install using local repo"
+  echo "      --help                           Display this help message"
+}
+
 parse_args() {
   while [ $# -gt 0 ]; do
     case "$1" in
+    --help)
+      usage
+      exit 0
+    ;;
+    -l|--local)
+      if [ $# -lt 0 ]; then
+        echo "Missing argument for $1";
+        exit 1;
+      fi
+      SSHNP_LOCAL="$2"
+      shift 2
+    ;;
+    -d|--dev)
+      if [ $# -lt 0 ]; then
+        echo "Missing argument for $1";
+        exit 1;
+      fi
+      SSHNP_DEV_MODE="$2"
+      shift 2
+    ;;
     -f|--from)
       if [ $# -lt 0 ]; then
         echo "Missing argument for $1";
@@ -23,7 +52,7 @@ parse_args() {
       fi
       CLIENT_ATSIGN="$2"
       shift 2
-      ;;
+    ;;
     -t|--to)
       if [ $# -lt 0 ]; then
         echo "Missing argument for $1";
@@ -31,7 +60,7 @@ parse_args() {
       fi
       DEVICE_MANAGER_ATSIGN="$2"
       shift 2
-      ;;
+    ;;
     -h|--host)
       if [ $# -lt 0 ]; then
         echo "Missing argument for $1";
@@ -39,37 +68,58 @@ parse_args() {
       fi
       HOST_RENDEZVOUS_ATSIGN="$2"
       shift 2
-      ;;
+    ;;
     *)
       echo "Unknown argument: $1"
       exit 1
-      ;;
+    ;;
     esac
-    shift
   done
 
   if [ -z "$CLIENT_ATSIGN" ]; then
-    echo "Client atSign: ";
-    read -r CLIENT_ATSIGN;
+    read -rp "Client address: " CLIENT_ATSIGN;
   fi
 
   if [ -z "$DEVICE_MANAGER_ATSIGN" ]; then
-    echo "Device manager atSign: ";
-    read -r DEVICE_MANAGER_ATSIGN;
+    read -rp "Device manager address: " DEVICE_MANAGER_ATSIGN;
   fi
 
   if [ -z "$HOST_RENDEZVOUS_ATSIGN" ]; then
-    echo "Host rendezvous atSign: ";
-    read -r HOST_RENDEZVOUS_ATSIGN;
+    echo Pick your default region:
+    echo am: Americas
+    echo ap: Asia Pacific
+    echo eu: Europe
+    read -rp "region: " HOST_RENDEZVOUS_ATSIGN;
   fi
+
+  while [[ "$HOST_RENDEZVOUS_ATSIGN" != @rv_* ]]; do
+    case "$HOST_RENDEZVOUS_ATSIGN" in
+    [Aa][Mm]*)
+      HOST_RENDEZVOUS_ATSIGN="@rv_am"
+      ;;
+    [Ee][Uu]*)
+      HOST_RENDEZVOUS_ATSIGN="@rv_eu"
+      ;;
+    [Aa][Pp]*)
+      HOST_RENDEZVOUS_ATSIGN="@rv_ap"
+      ;;
+    esac
+    if [[ "$HOST_RENDEZVOUS_ATSIGN" != @rv_* ]]; then
+      echo "Invalid region: $HOST_RENDEZVOUS_ATSIGN"
+      read -rp "region: " HOST_RENDEZVOUS_ATSIGN;
+    fi
+  done
+  echo;
 
   norm_atsign CLIENT_ATSIGN
   norm_atsign DEVICE_MANAGER_ATSIGN
-  norm_atsign HOST_RENDEZVOUS_ATSIGN
 }
 
 parse_env() {
-  HOME_PATH=$(eval echo "~$SUDO_USER");
+  if [ -z "$SSHNP_USER" ]; then
+    SSHNP_USER="$USER";
+  fi
+  HOME_PATH=$(eval echo "~$SSHNP_USER");
 
   if [ -z "$SSHNP_VERSION" ]; then
     SSHNP_VERSION="latest";
@@ -132,23 +182,17 @@ make_dirs() {
            "$HOME_PATH/.sshnp" \
            "$HOME_PATH/.atsign/keys" \
            "$HOME_PATH/.atsign/temp" \
-           "$HOME_PATH/.local/bin" \
-           "/usr/local/bin";
-
-  chown -R "$SUDO_USER" "$HOME_PATH/.ssh" \
-                                   "$HOME_PATH/.sshnp" \
-                                   "$HOME_PATH/.atsign" \
-                                   "$HOME_PATH/.local/bin";
-
-  chmod -R 700 "$HOME_PATH/.local/bin"\
-               "$HOME_PATH/.ssh" \
-               "$HOME_PATH/.sshnp" \
-               "$HOME_PATH/.atsign/keys";
+           "$HOME_PATH/.local/bin";
 }
 
 download() {
-  echo "Downloading $BINARY_NAME from $DOWNLOAD";
-  curl -sL "$DOWNLOAD" -o "$HOME_PATH/.atsign/temp/$BINARY_NAME.$EXT";
+  if [ -n "$SSHNP_LOCAL" ]; then
+    echo "DEV MODE: Installing using local $EXT file: $SSHNP_LOCAL";
+    cp "$SSHNP_LOCAL" "$HOME_PATH/.atsign/temp/$BINARY_NAME.$EXT";
+  else
+    echo "Downloading $BINARY_NAME from $DOWNLOAD";
+    curl -sL "$DOWNLOAD" -o "$HOME_PATH/.atsign/temp/$BINARY_NAME.$EXT";
+  fi
 
   case "$EXT" in
   zip)
@@ -158,14 +202,25 @@ download() {
     tar -zxvf "$HOME_PATH/.atsign/temp/$BINARY_NAME.$EXT" -C "$HOME_PATH/.atsign/temp/";
     ;;
   esac
-  if [ $SSHNP_DEV_MODE ]; then
-    echo "DEV MODE: using local templates";
-    cp -R templates/ "$HOME_PATH/.atsign/temp/$BINARY_NAME/templates/";
+  if [ -n "$SSHNP_DEV_MODE" ]; then
+    echo "DEV MODE: Installing from local repo: $SSHNP_DEV_MODE";
+    cp -R "$SSHNP_DEV_MODE/templates" "$HOME_PATH/.atsign/temp/$BINARY_NAME/";
+    cp -R "$SSHNP_DEV_MODE/scripts/" "$HOME_PATH/.atsign/temp/$BINARY_NAME/";
   fi
 }
 
-# Place user based scripts locally
-setup_local() {
+# Place the actual sshnp binary
+setup_main_binaries() {
+  MAIN_BINARIES="$BINARY_NAME at_activate sshrv update_sshnp";
+  for binary in $MAIN_BINARIES; do
+    mv "$HOME_PATH/.atsign/temp/$BINARY_NAME/$binary" "$HOME_PATH/.local/bin/$binary";
+    chmod +x "$HOME_PATH/.local/bin/$binary";
+  done
+  echo "Installed binaries: $MAIN_BINARIES";
+}
+
+# Place custom user based scripts
+setup_custom_binary() {
   echo "Installing $BINARY_NAME$DEVICE_MANAGER_ATSIGN to $HOME_PATH/.local/bin/$BINARY_NAME$DEVICE_MANAGER_ATSIGN";
   sed -e "s/\$CLIENT_ATSIGN/$CLIENT_ATSIGN/g" \
       -e "s/\$DEVICE_MANAGER_ATSIGN/$DEVICE_MANAGER_ATSIGN/g" \
@@ -175,38 +230,29 @@ setup_local() {
   chmod +x "$HOME_PATH/.local/bin/$BINARY_NAME$DEVICE_MANAGER_ATSIGN";
 }
 
-# Place the actual sshnp binary for global access
-setup_global() {
-  GLOBAL_BINARIES="$BINARY_NAME at_activate";
-  for binary in $GLOBAL_BINARIES; do
-    echo "Installing $binary to /usr/local/bin/$binary";
-    mv "$HOME_PATH/.atsign/temp/$BINARY_NAME/$binary" "/usr/local/bin/$binary";
-    chmod +x "/usr/local/bin/$binary";
-  done
-}
-
-cleanup() {
-  rm -rf "$HOME_PATH/.atsign/temp";
-}
-
 post_install() {
-  echo "$BINARY_NAME installed."
+  rm -rf "$HOME_PATH/.atsign/temp";
+  echo;
+
+  if ! echo "$PATH" | grep -q "$HOME_PATH/.local/bin"; then
+    PATH="\$PATH:$HOME_PATH/.local/bin";
+    echo "Added $HOME_PATH/.local/bin to your PATH."
+    echo "Include the following line in your shell profile to persist across logins:"
+    echo "  export PATH=\"\$PATH:$HOME_PATH/.local/bin\""
+  fi
+
+  echo; echo "Installation complete!";
 }
 
 # Wrapping install steps prevents issues caused by interrupting the download
 main () {
-  if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root";
-    exit 1;
-  fi
   parse_env
   make_dirs
   download
 
-  setup_local
-  setup_global
+  setup_main_binaries
+  setup_custom_binary
 
-  cleanup
   post_install
 }
 
