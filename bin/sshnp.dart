3e8e78e5c75cd7e0b9fde0d90a23f3972bdf3462
gkc <gary.casey@gmail.com>
2023-06-10 11:31:42 +0100

    style: removed a line of duplicate code from sshnp.dart

--- bin/sshnp.dart
+++ bin/sshnp.dart
@@ -241,8 +241,6 @@ void main(List<String> args) async {
 
   atClient = AtClientManager.getInstance().atClient;
 
-  atClient = AtClientManager.getInstance().atClient;
-
   NotificationService notificationService = atClient.notificationService;
 
   notificationService
