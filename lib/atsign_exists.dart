9cb7738ab15701bf88b8fb17204e10490a8300ef
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-17 00:06:19 -0400

    refactor: `atsign_exists.dart` spacings

--- lib/atsign_exists.dart
+++ lib/atsign_exists.dart
@@ -1,24 +1,22 @@
-
-
 import 'package:at_client/at_client.dart';
 
 /// Checks if the provided atSign's atServer has been properly activated with a public RSA key.
 /// `atClient` must be authenticated
 /// `atSign` is the atSign to check
 Future<bool> atSignIsActivated(final AtClient atClient, String atSign) async {
-    final Metadata metadata = Metadata()
-        ..isPublic = true
-        ..namespaceAware = false;
+  final Metadata metadata = Metadata()
+    ..isPublic = true
+    ..namespaceAware = false;
 
-    final AtKey publicKey = AtKey()
-        ..sharedBy = atSign
-        ..key = 'publickey'
-        ..metadata = metadata;
+  final AtKey publicKey = AtKey()
+    ..sharedBy = atSign
+    ..key = 'publickey'
+    ..metadata = metadata;
 
-    try {
-      await atClient.get(publicKey);
-      return true;
-    } catch (e) {
-      return false;
-    }
+  try {
+    await atClient.get(publicKey);
+    return true;
+  } catch (e) {
+    return false;
   }
+}
