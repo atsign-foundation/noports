581b0f0f22981246f3c3d045ed834413c0a3f170
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-27 00:50:15 -0400

    feat: start on e2e testing sshnp dart project

---
+++ test/end2end_tests/images/base/build.sh
@@ -0,0 +1,2 @@
+sudo docker image rm atsigncompany/end2end_test_base
+sudo docker build -t atsigncompany/end2end_test_base -f ./Dockerfile ./
---
+++ test/end2end_tests/test/end2end_tests_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(42,42);
+  });
+}
