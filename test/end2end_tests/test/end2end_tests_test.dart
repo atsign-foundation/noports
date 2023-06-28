f964a578ab5d6c801b6b55fef33fb0ce4a353a04
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-28 00:07:36 -0400

    ci: poop

--- test/end2end_tests/test/end2end_tests_test.dart
+++ test/end2end_tests/test/end2end_tests_test.dart
@@ -1,7 +1,21 @@
+import 'package:sshnoports/sshnp.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('sshnoports', () {
-    expect(1, 1);
+  test('sshnoports', () async {
+    const List<String> args = [
+      '-f',
+      '@jeremy_0',
+      '-t',
+      '@soccer0',
+      '-s',
+      'id_ed25519.pub',
+      '-h',
+      '@rv_am'
+    ];
+
+    SSHNP sshnp = await SSHNP.fromCommandLineArgs(args);
+    await sshnp.init();
+    await sshnp.run();
   });
 }
