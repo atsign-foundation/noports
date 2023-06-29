3c57ca5147b9033ceb56fe35dc83331d883e17cb
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-29 18:54:18 -0400

    ci:

--- test/end2end_tests/context/sshnpd/entrypoint.sh
+++ test/end2end_tests/context/sshnpd/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo -u atsign service ssh start
 cd ~/.local/bin
 sudo -u atsign ./sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v > results.txt
 sudo -u atsign cat results.txt
