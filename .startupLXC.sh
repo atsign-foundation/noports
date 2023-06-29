c8b1b2d81b74e8957573db77ad23f58f10c1415d
colin constable <colin@constable.cc>
2023-06-29 12:29:07 -0700

    feat: move to alpine dart

--- .startupLXC.sh
+++ .startupLXC.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ssh-keygen -A
-dhclient
+#dhclient
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
+cd /app
+sudo -u atsign dart pub get -C /app
 while true
 do
-sudo -u atsign /usr/local/at/sshnpd $*
+sudo -u atsign dart run /app/bin/sshnpd.dart $*
 sleep 3
 done
