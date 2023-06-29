cd640c9f86f2d8f15357eb6295bd1c8bc4c98947
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-29 17:21:45 -0400

    ci:

--- test/end2end_tests/context/sshnp/entrypoint.sh
+++ test/end2end_tests/context/sshnp/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/.local/bin
 ./sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub -v > results.txt
-exit 1
+cat results.txt
--- test/end2end_tests/context/sshnpd/entrypoint.sh
+++ test/end2end_tests/context/sshnpd/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/.local/bin
 ./sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v > results.txt
-exit 1
+cat results.txt
