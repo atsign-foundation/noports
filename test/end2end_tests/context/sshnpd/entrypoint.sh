d0f2164bcd6a689573515d02c82ab234448c91af
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-29 19:28:08 -0400

    ci: remove sudo -u for cd

--- test/end2end_tests/context/sshnpd/entrypoint.sh
+++ test/end2end_tests/context/sshnpd/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo -u atsign cd ~/.local/bin
+cd ~/.local/bin
 sudo -u atsign ./sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v > results.txt
 sudo -u atsign cat results.txt
