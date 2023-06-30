7c364b5a1ce3b9cc9fb1f605d5926523c24af9b2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-30 14:27:52 -0400

    ci:

--- test/end2end_tests/context/sshnp/entrypoint.sh
+++ test/end2end_tests/context/sshnp/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sleep 30
-~/.local/bin/sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub -v
-~/.local/bin/sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub -v
+sleep 5
 ~/.local/bin/sshnp -f @jeremy_0 -t @smoothalligator -d docker -h @rv_am -s id_ed25519.pub -v
