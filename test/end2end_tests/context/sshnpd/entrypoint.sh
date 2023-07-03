0ae4e8ebf3519b3416f24f460695eb182ff3b199
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-07-03 15:00:29 -0400

    ci:

--- test/end2end_tests/context/sshnpd/entrypoint.sh
+++ test/end2end_tests/context/sshnpd/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# tmux new-session -d -s sshnpd
-# tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v" C-m
-# sleep 2
-
-~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v
+tmux new-session -d -s sshnpd
+tmux send-keys -t sshnpd "~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v" C-m
 sleep 2
+
+# ~/.local/bin/sshnpd -a @smoothalligator -m @jeremy_0 -d docker -s -u -v
+# sleep 2
