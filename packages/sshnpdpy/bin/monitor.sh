6deb12338933aca1dae1ccb2df1ed3875794c019
Xavier Lin <xavier.li.1332@gmail.com>
2023-07-06 11:33:25 -0400

    feat: monitor now sends pkam (i renamed it again)

--- packages/sshnpdpy/bin/monitor.sh
+++ packages/sshnpdpy/bin/monitor.sh
@@ -16,24 +16,33 @@ root_answer="$({ echo "$atsign" ; sleep 1 ; echo "Q"; } | openssl s_client -brie
 
 #regex for server address
 regex='@[a-fA-F0-9-]+\.[a-zA-Z0-9.-]+:[0-9]+'
- 
+
 if [[ $root_answer =~ $regex ]]; then
   server_address="${BASH_REMATCH[0]:1}"
 else
   echo "No match found."
 fi
 
-#same thing to start authentication into server
-server_answer="$({ echo "from:$atsign" ; sleep 1 ; echo "Q"; } | openssl s_client -brief -connect $server_address 2>&1)"
 
-if [[ $server_answer =~ @data:(.*) ]]; then
-  temp="${BASH_REMATCH[1]}"
-else
-  echo "Pattern not found."
-fi
+input_command(){
+  echo "from:$atsign"
+  sleep 3
+  while read -r line; do
+    echo "$line"
+  done < <(tail -f session_ouput.txt)
+  sleep 3
+  read -r signature
+  echo "$signature"
+  echo "monitor"
+}
 
-result=$(echo "$temp" | sed 's/@DONE//g')
 
-#returns passing challenge and server address
-echo "$result|$server_address"
+input_command | openssl s_client -brief -connect $server_address > session_ouput.txt 2>&1 &
+
+
+ while read -r line; do
+    echo "$line"
+  done < <(tail -f session_ouput.txt)
+
+
 
--- packages/sshnpdpy/lib/sshnpd.py
+++ packages/sshnpdpy/lib/sshnpd.py
@@ -12,10 +12,9 @@
 ##  let's get this mvp out.
 import os, argparse, subprocess, base64
 
-from Cryptodome import Random
-from  Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import PKCS1_OAEP
 
+from at_client.common import AtClient, AtSign
+from at_client.util import EncryptionUtil, KeysUtil
 
 namespace = 'sshnp'
 parser = argparse.ArgumentParser("sshnpd")
@@ -35,16 +34,41 @@ else:
 
 commit_log_path = os.path.dirname(f'{home_dir}/.sshnp/{args.device_atsign}/storage/commitLog')
 download_path = os.path.dirname(f'{home_dir}/.sshnp/files')
-keys_path = os.path.dirname(f'{home_dir}/.atsign/keys')
-bin_path = '/mnt/c/Users/xavie/atsign/sshnoports/packages/sshnpdpy/bin/challenge.sh' 
-process_response = subprocess.run([bin_path, args.manager_atsign], capture_output=True, text=True)
-process_output = process_response.stdout.split('|')
-challenge = process_output[0]
-secondary_address = process_output[1]
+#
+bin_path = '/mnt/c/Users/xavie/atsign/sshnoports/packages/sshnpdpy/bin/' 
+def monitor_process(popen):    
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, [f'{bin_path}monitor.sh', args.manager_atsign[1:]])
+    
+challenge = ""
+challenge_recieved = False
+ssh_notification_recieved = False
+
+popen = subprocess.Popen([f'{bin_path}monitor.sh', args.manager_atsign[1:]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+while not ssh_notification_recieved:
+    if not challenge_recieved:
+        for line in monitor_process(popen):
+            if "data" in line:
+                challenge = line.split("@data:")[1].strip()    
+                print(challenge)
+                challenge_recieved = True
+                break
+    else:    
+        client = AtClient(AtSign(args.manager_atsign))
+        client.pkam_authenticate()
+        KeysUtil.load_keys(args.manager_atsign)
+        privatekey = client.keys[KeysUtil.pkam_private_key_name]
+        signature = EncryptionUtil.sign_sha256_rsa(challenge, privatekey)
+        popen.stdin.write(f'pkam:{signature}\n')
+        popen.stdin.flush()
+        for notification in monitor_process(popen):
+            print(notification)  
 
 
-print(challenge)
-print(secondary_address)
 
 
 
