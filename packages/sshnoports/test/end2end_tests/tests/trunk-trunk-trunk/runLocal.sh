606e6289a2c629b5e1848d2823c619ace129e47d
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-07-06 22:03:17 -0400

    ci: lomao

--- packages/sshnoports/test/end2end_tests/tests/trunk-trunk-trunk/runLocal.sh
+++ packages/sshnoports/test/end2end_tests/tests/trunk-trunk-trunk/runLocal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker compose build --build-arg branch=trunk --no-cache
-sudo docker compose up --abort-on-container-exit --exit-code-from=container-trunk-sshnp
+sudo docker compose build --no-cache
+sudo docker compose up --abort-on-container-exit --exit-code-from=container-sshnp
 sudo docker compose down
