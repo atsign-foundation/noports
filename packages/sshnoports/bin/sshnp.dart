73a9568a5ed0068b77d7bc7c027094c1725bfe17
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-07-11 11:48:55 -0400

    test: should pass

--- packages/sshnoports/bin/sshnp.dart
+++ packages/sshnoports/bin/sshnp.dart
@@ -9,8 +9,6 @@ import 'package:sshnoports/sshnp.dart';
 import 'package:sshnoports/cleanup_sshnp.dart';
 
 void main(List<String> args) async {
-  // test breaking sshnp TODO : temorary, delete later
-  exit(1);
   AtSignLogger.root_level = 'SHOUT';
 
   SSHNP sshnp = await SSHNP.fromCommandLineArgs(args);
