8f247ab6186aadf570d64b199bdcf4c005f2d31d
xavierchanth <xchanthavong@gmail.com>
2023-08-14 02:53:26 +0800

    ci: remove -v from legacy command

--- tests/end2end_tests/templates/sshnp_entrypoint.sh
+++ tests/end2end_tests/templates/sshnp_entrypoint.sh
@@ -8,7 +8,7 @@ else
     SSHNP_CUSTOM_FILE_VERSION="$(sed -e '2!d' "$HOME"/.local/bin/sshnp@sshnpdatsign | cut -d'v' -f2)"
     case "$SSHNP_CUSTOM_FILE_VERSION" in
     1*)
-        SSHNP_COMMAND="$HOME/.local/bin/sshnp@sshnpdatsign -h @sshrvdatsign deviceName -v > logs.txt"
+        SSHNP_COMMAND="$HOME/.local/bin/sshnp@sshnpdatsign -h @sshrvdatsign deviceName > logs.txt"
         ;;
     *)
         SSHNP_COMMAND="$HOME/.local/bin/sshnp@sshnpdatsign -h @sshrvdatsign -d deviceName -s id_ed25519.pub -v > logs.txt"
