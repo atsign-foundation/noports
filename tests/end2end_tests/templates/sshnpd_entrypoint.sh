7aa06a5652883691261f69e6a527619e1d4e4281
xavierchanth <xchanthavong@gmail.com>
2023-08-14 10:36:03 +0800

    ci: wait for sshnp to connect

--- tests/end2end_tests/templates/sshnpd_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_entrypoint.sh
@@ -2,7 +2,7 @@
 if [ -z "$USE_INSTALLER" ]; then
   SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
 else
-    SSHNPD_COMMAND="bash"
+    SSHNPD_COMMAND="sleep 60"
 fi
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
