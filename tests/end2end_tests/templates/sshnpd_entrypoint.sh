a1b06cf7d1d7106c2095abdc3ac961c96a6af382
xavierchanth <xchanthavong@gmail.com>
2023-08-14 10:30:35 +0800

    ci: fix sshnpd_entrypoint

--- tests/end2end_tests/templates/sshnpd_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_entrypoint.sh
@@ -2,7 +2,7 @@
 if [ -z "$USE_INSTALLER" ]; then
   SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
 else
-    SSHNPD_COMMAND=true # noop
+    SSHNPD_COMMAND="bash"
 fi
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
