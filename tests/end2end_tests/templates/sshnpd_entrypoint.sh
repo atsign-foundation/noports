d259dae352749688d5e882645f8c8d7ece5ac275
xavierchanth <xchanthavong@gmail.com>
2023-08-14 11:46:42 +0800

    ci: fix Dockerfile config

--- tests/end2end_tests/templates/sshnpd_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
+if [ -z "$USE_INSTALLER" ]; then
+  SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
+else
+  SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign"
+fi
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
