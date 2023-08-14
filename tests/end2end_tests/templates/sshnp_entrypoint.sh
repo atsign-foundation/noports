2c39c3965cf8f9334475664448927029b3254457
xavierchanth <xchanthavong@gmail.com>
2023-08-14 11:33:59 +0800

    ci: tweaking e2e tests

--- tests/end2end_tests/templates/sshnp_entrypoint.sh
+++ tests/end2end_tests/templates/sshnp_entrypoint.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 sleep WAITING_TIME # time for sshnpd to share device name
 
-if [ -z "$USE_INSTALLER" ]; then
-    SSHNP_COMMAND="$HOME/.local/bin/sshnp -f @sshnpatsign -t @sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > logs.txt"
-else
-    SSHNP_CUSTOM_FILE_VERSION="$(sed -e '2!d' "$HOME"/.local/bin/sshnp@sshnpdatsign | cut -d'v' -f2)"
-    case "$SSHNP_CUSTOM_FILE_VERSION" in
-    1*)
-        # Version 1 of the file cannot be tested in this environment since the
-        # keys are cut separately and cannot be passed into this version of the file
-        # SSHNP_COMMAND="\$($HOME/.local/bin/sshnp@sshnpdatsign deviceName) > logs.txt"
-
-        SSHNP_COMMAND="$HOME/.local/bin/sshnp -f @sshnpatsign -t @sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > logs.txt"
-        ;;
-    *)
-        SSHNP_COMMAND="$HOME/.local/bin/sshnp@sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > logs.txt"
-        ;;
-    esac
-fi
+SSHNP_COMMAND="$HOME/.local/bin/sshnp -f @sshnpatsign -t @sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > logs.txt"
 echo "Running: $SSHNP_COMMAND"
 eval "$SSHNP_COMMAND"
 cat logs.txt
@@ -36,7 +20,7 @@ if [ ! -s sshcommand.txt ]; then
         exit 1
     fi
 fi
-echo -n " -o StrictHostKeyChecking=no " >> sshcommand.txt ;
+echo "$(sed '1!d' sshcommand.txt) -o StrictHostKeyChecking=no " > sshcommand.txt ;
 echo "ssh -p command: $(cat sshcommand.txt)"
 echo "sh test.sh " | eval "$(cat sshcommand.txt)"
 sleep 2 # time for ssh connection to properly exit
--- tests/end2end_tests/templates/sshnpd_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_entrypoint.sh
@@ -2,10 +2,7 @@
 if [ -z "$USE_INSTALLER" ]; then
   SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
 else
-  crontab -l
-  tmux ls
-  ps aux | grep sshnpd
-  SSHNPD_COMMAND="sleep 60"
+  SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign"
 fi
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
