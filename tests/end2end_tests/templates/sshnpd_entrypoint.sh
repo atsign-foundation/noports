59fffed90f67aea75e69d253db626cceff3fe91f
xavierchanth <xchanthavong@gmail.com>
2023-08-14 10:49:27 +0800

    ci: debug sshnpd_entrypoint

--- tests/end2end_tests/templates/sshnpd_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_entrypoint.sh
@@ -2,7 +2,10 @@
 if [ -z "$USE_INSTALLER" ]; then
   SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v"
 else
-    SSHNPD_COMMAND="sleep 60"
+  crontab -l
+  tmux ls
+  ps aux | grep sshnpd
+  SSHNPD_COMMAND="sleep 60"
 fi
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
