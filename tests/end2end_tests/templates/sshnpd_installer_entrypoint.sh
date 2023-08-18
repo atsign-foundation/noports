9df56cebe4535110533888411b86c50b5aa372d1
xavierchanth <xchanthavong@gmail.com>
2023-08-18 20:35:12 +0800

    fix: call -s and -u in the correct place for sshnpd installer test

--- tests/end2end_tests/templates/sshnpd_installer_entrypoint.sh
+++ tests/end2end_tests/templates/sshnpd_installer_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign -s -u"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
