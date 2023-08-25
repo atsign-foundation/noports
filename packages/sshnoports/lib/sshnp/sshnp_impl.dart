ed1583b3b7fb1171cb788ba516d919126ee878f6
xavierchanth <xchanthavong@gmail.com>
2023-08-25 17:54:14 +0800

    fix: use loopbackIPV4 address for reverse ssh host

--- packages/sshnoports/lib/sshnp/sshnp_impl.dart
+++ packages/sshnoports/lib/sshnp/sshnp_impl.dart
@@ -528,7 +528,7 @@ class SSHNPImpl implements SSHNP {
       return SSHCommand.base(
         localPort: localPort,
         remoteUsername: username,
-        host: host,
+        host: InternetAddress.loopbackIPv4.address,
         privateKeyFileName: publicKeyFileName,
       );
     }
@@ -566,7 +566,7 @@ class SSHNPImpl implements SSHNP {
       return SSHCommand.base(
         localPort: localPort,
         remoteUsername: username,
-        host: host,
+        host: InternetAddress.loopbackIPv4.address,
         privateKeyFileName: publicKeyFileName,
       );
     }
