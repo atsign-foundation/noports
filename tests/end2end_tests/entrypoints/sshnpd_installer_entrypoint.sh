bb4a90b6c34076abc248cdceea858aacf9eb9f67
xavierchanth <xchanthavong@gmail.com>
2023-08-25 16:05:37 +0800

    ci: check in local e2e tests via run via act emulation

--- tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign > logs.txt"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign 2> err.txt"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
--- tools/local-e2e-test/run.sh
+++ tools/local-e2e-test/run.sh
@@ -4,9 +4,13 @@ FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 PROJECT_ROOT="$SCRIPT_DIRECTORY/../.."
 
+DOCKER_HOST="$(docker context inspect -f '{{.Endpoints.docker.Host}}')"
+env DOCKER_HOST="$DOCKER_HOST" \
 act \
 -W "$PROJECT_ROOT/.github/workflows/end2end_tests.yaml" \
 -j e2e_test \
 --env-file "$SCRIPT_DIRECTORY/.env" \
 --secret-file "$SCRIPT_DIRECTORY/.secrets" \
+--container-architecture linux/amd64 \
+--bind \
 "$@"
