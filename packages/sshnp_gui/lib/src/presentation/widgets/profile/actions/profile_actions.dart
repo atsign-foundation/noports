dc4eccb099ce0d1b24606b3466e786412f5edfde
xavierchanth <xchanthavong@gmail.com>
2023-09-07 12:12:27 +0800

    chore: update profile_actions

--- packages/sshnp_gui/lib/src/presentation/widgets/profile/actions/profile_actions.dart
+++ packages/sshnp_gui/lib/src/presentation/widgets/profile/actions/profile_actions.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sshnoports/sshnp/sshnp.dart';
-import 'package:sshnp_gui/src/presentation/widgets/home_screen_table/custom_table_cell.dart';
 import 'package:sshnp_gui/src/presentation/widgets/profile/actions/profile_delete_action.dart';
 import 'package:sshnp_gui/src/presentation/widgets/profile/actions/profile_edit_action.dart';
 import 'package:sshnp_gui/src/presentation/widgets/profile/actions/profile_run_action.dart';
 
-class HomeScreenTableActions extends StatelessWidget {
-  final AsyncValue<SSHNPParams> params;
-  const HomeScreenTableActions(this.params, {Key? key}) : super(key: key);
+class ProfileActions extends StatelessWidget {
+  final SSHNPParams params;
+  const ProfileActions(this.params, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return params.when(
-      data: (p) => CustomTableCell(
-        child: Row(
-          children: [
-            ProfileRunAction(p),
-            ProfileEditAction(p),
-            ProfileDeleteAction(p),
-          ],
-        ),
-      ),
-      error: (e, s) => const CustomTableCell.text(text: 'Error fetching data...'),
-      loading: () => const CustomTableCell.text(text: '...'),
+    return Row(
+      children: [
+        ProfileRunAction(params),
+        ProfileEditAction(params),
+        ProfileDeleteAction(params),
+      ],
     );
   }
 }
