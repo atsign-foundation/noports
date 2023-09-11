3bddc44a4ced3f2eeb8507ec3f482566702eb5ee
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-09-11 12:03:44 -0400

    ci: use `&>` and refactor to all.txt instead of err.txt

--- tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign 2> err.txt"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign &> all.txt"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
