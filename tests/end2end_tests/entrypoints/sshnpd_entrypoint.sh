8e5aa5c090cafbbf84e4830abade1cd31ae87435
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-09-12 10:23:19 -0400

    ci: use posix compilation notation `2>&>`

--- tests/end2end_tests/entrypoints/sshnpd_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnpd_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "SSHNPD START ENTRY"
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v &> all.txt"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v 2>&> all.txt"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
