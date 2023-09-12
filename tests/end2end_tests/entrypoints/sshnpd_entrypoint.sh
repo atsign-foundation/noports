870f4f7ed870a7c5cbb144ab3c22e5202df7ed12
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-09-12 18:59:23 -0400

    ci:

--- tests/end2end_tests/entrypoints/sshnpd_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnpd_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "SSHNPD START ENTRY"
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v > all.txt 2>&1"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v >> all.txt 2>&1"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
 tail -f all.txt
--- tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnpd_installer_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign > all.txt 2>&1"
+SSHNPD_COMMAND="$HOME/.local/bin/sshnpd@sshnpatsign >> all.txt 2>&1"
 echo "Running: $SSHNPD_COMMAND"
 eval "$SSHNPD_COMMAND"
 tail -f all.txt
