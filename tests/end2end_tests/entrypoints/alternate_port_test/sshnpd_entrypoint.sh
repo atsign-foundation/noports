085edcf8c9a6cc34284b0a152f1b30e8f8fb092c
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-09-12 20:00:09 -0400

    ci: just use regular npd entrypoint

--- tests/end2end_tests/entrypoints/alternate_port_test/sshnpd_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "SSHNPD START ENTRY"
-SSHNPD_COMMAND="$HOME/.local/bin/sshnpd -a @sshnpdatsign -m @sshnpatsign -d deviceName -s -u -v 2>&1 | tee all.txt"
-echo "Running: $SSHNPD_COMMAND"
-eval "$SSHNPD_COMMAND"
