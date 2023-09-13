92be4ab503002655f30352931f438538080af35e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-09-13 16:27:19 -0400

    refactor: sshnp/npd/rvd.log

--- tests/end2end_tests/entrypoints/sshnp_installer_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshnp_installer_entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 sleep WAITING_TIME # time for sshnpd to share device name
 
-SSHNP_COMMAND="$HOME/.local/bin/sshnp -f @sshnpatsign -t @sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > logs.txt"
+SSHNP_COMMAND="$HOME/.local/bin/sshnp -f @sshnpatsign -t @sshnpdatsign -d deviceName -h @sshrvdatsign -s id_ed25519.pub -v > sshnp.log"
 echo "Running: $SSHNP_COMMAND"
 eval "$SSHNP_COMMAND"
-cat logs.txt
-tail -n 5 logs.txt | grep "ssh -p" > sshcommand.txt
+cat sshnp.log
+tail -n 5 sshnp.log | grep "ssh -p" > sshcommand.txt
 
 if [ ! -s sshcommand.txt ]; then
     # try again
     echo "Running: $SSHNP_COMMAND"
     eval "$SSHNP_COMMAND"
-    cat logs.txt
-    tail -n 5 logs.txt | grep "ssh -p" > sshcommand.txt
+    cat sshnp.log
+    tail -n 5 sshnp.log | grep "ssh -p" > sshcommand.txt
     if [ ! -s sshcommand.txt ]; then
-        echo "could not find 'ssh -p' command in logs.txt"
-        echo "last 5 lines of logs.txt:"
-        tail -n 5 logs.txt || echo
+        echo "could not find 'ssh -p' command in sshnp.log"
+        echo "last 5 lines of sshnp.log:"
+        tail -n 5 sshnp.log || echo
         exit 1
     fi
 fi
--- tests/end2end_tests/entrypoints/sshrvd_entrypoint.sh
+++ tests/end2end_tests/entrypoints/sshrvd_entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-"$HOME"/.local/bin/sshrvd -a @sshrvdatsign -i "$(hostname -i)" -v -s
-sleep 60 # sleep 60 because other containers depend on it. And if it's not being used (let's say you're using @rv_am), then it will just sleep until sshnp exits
-exit 0
+"$HOME"/.local/bin/sshrvd -a @sshrvdatsign -i "$(hostname -i)" -v -s 2>&1 | tee -a sshrvd.log
