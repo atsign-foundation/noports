387ddbf5a6b85cbc8a8bee98af8f120dc41f07d2
xavierchanth <xchanthavong@gmail.com>
2023-09-14 17:13:52 +0800

    chore: set version to 4.0.0-rc.4

--- packages/sshnoports/lib/version.dart
+++ packages/sshnoports/lib/version.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 // Note: if you update this version also update pubspec.yaml
-const String version = "4.0.0-rc.3";
+const String version = "4.0.0-rc.4";
 
 /// Print version number
 void printVersion() {
