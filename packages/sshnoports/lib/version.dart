58ff1a2917e23b2e30468aa007d3804a9c0ad079
xavierchanth <xchanthavong@gmail.com>
2023-09-29 08:51:35 -0400

    ci: add better version tag checker

--- packages/sshnoports/lib/version.dart
+++ packages/sshnoports/lib/version.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 // Note: if you update this version also update pubspec.yaml
-const String version = "4.0.0-rc.6";
+const String version = "4.0.0-rc.7";
 
 /// Print version number
 void printVersion() {
