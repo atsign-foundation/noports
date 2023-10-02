5c92c4fe4f287fbeb8e0d8282c2a95ed607e5f95
xavierchanth <xchanthavong@gmail.com>
2023-10-02 17:33:40 -0400

    chore: revert end2end test regression

--- tests/end2end_tests/contexts/_init_/setup-sshnp-entrypoint.sh
+++ tests/end2end_tests/contexts/_init_/setup-sshnp-entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# this script copies the template sshnp entrypoint to ../sshnp/entrypoint.sh
-# then also replaces the device name, sshnp atSign, sshnpd atSign, and sshrvd atSign with the provided arguments
-# example usage: ./setup-sshnp-entrypoint.sh e2e @alice @alice @alice
-
 device=$1 # e.g. e2e
 sshnp=$2 # e.g. @alice
 sshnpd=$3 # e.g. @alice
 sshrvd=$4 # e.g. @alice
 template_name=$5 # e.g. sshnp_entrypoint.sh
+args=$6 # e.g. -v
 
 cp ../../entrypoints/"$template_name" ../sshnp/entrypoint.sh # copy template to the mounted folder
 
@@ -24,3 +21,4 @@ eval "$prefix" "s/@sshnpatsign/${sshnp}/g" ../sshnp/entrypoint.sh
 eval "$prefix" "s/@sshnpdatsign/${sshnpd}/g" ../sshnp/entrypoint.sh
 eval "$prefix" "s/@sshrvdatsign/${sshrvd}/g" ../sshnp/entrypoint.sh
 eval "$prefix" "s/deviceName/${device}/g" ../sshnp/entrypoint.sh
+eval "$prefix" "s/args/${args}/g" ../sshnp/entrypoint.sh
