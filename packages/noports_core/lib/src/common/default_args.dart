6f068b0690a71970c6da9d7eb4869ec280762ef7
xavierchanth <xchanthavong@gmail.com>
2023-11-06 13:26:22 -0500

    chore: remove unused line

--- packages/noports_core/lib/src/common/default_args.dart
+++ packages/noports_core/lib/src/common/default_args.dart
@@ -8,7 +8,6 @@ class DefaultArgs {
   static const SupportedSshAlgorithm sshAlgorithm =
       SupportedSshAlgorithm.ed25519;
   static const bool verbose = false;
-  static const SupportedSshAlgorithm algorithm = SupportedSshAlgorithm.ed25519;
   static const String rootDomain = 'root.atsign.org';
   static const SshrvGenerator sshrvGenerator = Sshrv.exec;
   static const int localSshdPort = 22;
