0c2105d6d7144ba0e7b7ca0762cb27c09905fed8
xavierchanth <xchanthavong@gmail.com>
2023-11-14 15:14:19 -0500

    test: add sshnp public api test

--- packages/noports_core/test/sshnp/sshnp_test.dart
+++ packages/noports_core/test/sshnp/sshnp_test.dart
@@ -1,7 +1,46 @@
+import 'package:at_client/at_client.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:noports_core/sshnp_foundation.dart';
 import 'package:test/test.dart';
+
+class StubbedSshnp extends Mock implements Sshnp {}
+
+class MockAtClient extends Mock implements AtClient {}
+
+class MockSshnpParams extends Mock implements SshnpParams {}
+
+class MySshnpResult extends SshnpResult {}
 
 void main() {
   group('Sshnp', () {
-    test('public API test', () {});
-  });
+    late final StubbedSshnp stubbedSshnp;
+    late final MockAtClient mockAtClient;
+    late final MockSshnpParams mockSshnpParams;
+    late final MySshnpResult mySshnpResult;
+    late final SshnpDeviceList sshnpDeviceList;
+
+    setUp(() {
+      stubbedSshnp = StubbedSshnp();
+      mockAtClient = MockAtClient();
+      mockSshnpParams = MockSshnpParams();
+      mySshnpResult = MySshnpResult();
+      sshnpDeviceList = SshnpDeviceList();
+    });
+
+    test('public API test', () async {
+      /// Just test that the public members are there,
+      /// implementation specific factories will be tested as part of the
+      /// implementation specific tests.
+      when(() => stubbedSshnp.atClient).thenReturn(mockAtClient);
+      when(() => stubbedSshnp.params).thenReturn(mockSshnpParams);
+      when(() => stubbedSshnp.run()).thenAnswer((_) async => mySshnpResult);
+      when(() => stubbedSshnp.listDevices())
+          .thenAnswer((_) async => sshnpDeviceList);
+
+      expect(stubbedSshnp.atClient, mockAtClient);
+      expect(stubbedSshnp.params, mockSshnpParams);
+      await expectLater(await stubbedSshnp.run(), mySshnpResult);
+      await expectLater(await stubbedSshnp.listDevices(), sshnpDeviceList);
+    });
+  }); // group Sshnp
 }
