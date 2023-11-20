f9eff5a706d8f66f7f3443fe8a280b96a378ca54
xavierchanth <xchanthavong@gmail.com>
2023-11-20 16:03:23 -0500

    test: sshrvd channel implementations

--- packages/noports_core/test/sshnp/util/sshrvd_channel/sshrvd_dart_channel_test.dart
+++ packages/noports_core/test/sshnp/util/sshrvd_channel/sshrvd_dart_channel_test.dart
@@ -1,1 +1,30 @@
-void main() {}
+import 'package:noports_core/sshnp_foundation.dart';
+import 'package:socket_connector/socket_connector.dart';
+import 'package:test/test.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../sshnp_mocks.dart';
+
+void main() {
+  group('SshrvdDartChannel', () {
+    late MockAtClient mockAtClient;
+    late MockSshnpParams mockSshnpParams;
+    late String sessionId;
+
+    setUp(() {
+      mockAtClient = MockAtClient();
+      mockSshnpParams = MockSshnpParams();
+      sessionId = Uuid().v4();
+    });
+    test('public API', () {
+      expect(
+        SshrvdDartChannel(
+          atClient: mockAtClient,
+          params: mockSshnpParams,
+          sessionId: sessionId,
+        ),
+        isA<SshrvdChannel<SocketConnector>>(),
+      );
+    });
+  }); // group SshrvdDartChannel
+}
