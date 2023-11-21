4d7662fecb9337e4104fc7d03b27cfddfe7cfae8
xavierchanth <xchanthavong@gmail.com>
2023-11-21 16:27:53 -0500

    refactor: simplify SshnpDartSshKeyHandler

--- packages/noports_core/lib/src/sshnp/util/sshnp_ssh_key_handler/sshnp_dart_ssh_key_handler.dart
+++ packages/noports_core/lib/src/sshnp/util/sshnp_ssh_key_handler/sshnp_dart_ssh_key_handler.dart
@@ -1,11 +1,10 @@
 import 'package:noports_core/sshnp_foundation.dart';
 
-mixin SshnpDartSshKeyHandler on SshnpCore implements SshnpKeyHandler {
+mixin SshnpDartSshKeyHandler implements SshnpKeyHandler {
   @override
   DartSshKeyUtil get keyUtil => _sshKeyUtil;
   final DartSshKeyUtil _sshKeyUtil = DartSshKeyUtil();
 
   @override
-  AtSshKeyPair? get identityKeyPair => _identityKeyPair;
-  AtSshKeyPair? _identityKeyPair;
+  AtSshKeyPair? identityKeyPair;
 }
