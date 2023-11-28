4c464c1e1e357b2cb9d4773cea0c1d1872ef06f5
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-11-28 12:42:02 -0400

    feat: support for mobile view added

--- packages/sshnp_gui/lib/src/presentation/screens/support_screen.dart
+++ packages/sshnp_gui/lib/src/presentation/screens/support_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:sshnp_gui/src/presentation/widgets/custom_list_tile.dart';
-import 'package:sshnp_gui/src/presentation/widgets/navigation/app_navigation_rail.dart';
-import 'package:sshnp_gui/src/utility/constants.dart';
-import 'package:sshnp_gui/src/utility/sizes.dart';
+import 'package:sshnp_gui/src/presentation/widgets/support_screen_widgets/support_screen_desktop_view.dart';
+import 'package:sshnp_gui/src/presentation/widgets/utility/responsive_widget.dart';
+
+import '../widgets/support_screen_widgets/support_screen_mobile_view.dart';
 
 class SupportScreen extends StatelessWidget {
   const SupportScreen({Key? key}) : super(key: key);
@@ -11,59 +10,10 @@ class SupportScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final strings = AppLocalizations.of(context)!;
-    return Scaffold(
-      body: SafeArea(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const AppNavigationRail(),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: Sizes.p36, top: Sizes.p21, right: Sizes.p36),
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: Sizes.p20),
-                    child: Text(
-                      strings.support,
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                  ),
-                  Text(
-                    strings.supportDescription,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: kTextColorDark),
-                  ),
-                  gapH30,
-                  const Flexible(
-                    child: Row(
-                      children: [
-                        Flexible(child: CustomListTile.discord()),
-                        Flexible(child: CustomListTile.email()),
-                      ],
-                    ),
-                  ),
-                  gapH20,
-                  const Divider(
-                    color: kProfileFormFieldColor,
-                  ),
-                  gapH20,
-                  const Flexible(
-                    child: Row(
-                      children: [
-                        Flexible(child: CustomListTile.faq()),
-                        Flexible(child: CustomListTile.privacyPolicy()),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      )),
+    return const ResponsiveWidget(
+      mobileScreen: SupportScreenMobileView(),
+      largeScreen: SupportScreenDesktopView(),
+      tabletScreen: SupportScreenDesktopView(),
     );
   }
 }
