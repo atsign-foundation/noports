14cdc9d34b8ab1466e9ac450bf0e58ea2406da47
vjag <jagannadh81@gmail.com>
2023-12-05 21:54:57 +0530

    Refactored code to make it test friendly and added few unit tests

--- packages/noports_core/test/sshrvd/notification_subscription_test.dart
+++ packages/noports_core/test/sshrvd/notification_subscription_test.dart
@@ -6,5 +6,6 @@ void main() {
   test('Test notification subscription regex', () {
     expect(RegExp(SshrvdImpl.subscriptionRegex).hasMatch('jagan@test.${Sshrvd.namespace}@jagan'), true);
     expect(RegExp(SshrvdImpl.subscriptionRegex).hasMatch('${Sshrvd.namespace}@'), true);
+    expect(RegExp(SshrvdImpl.subscriptionRegex).hasMatch('${Sshrvd.namespace}.test@'), false);
   });
 }
