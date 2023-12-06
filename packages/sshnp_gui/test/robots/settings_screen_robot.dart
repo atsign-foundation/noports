55f37c2632f3c845b2a0c044a450bb4d7e4fd98a
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-12-06 14:20:56 -0400

    chore: widget test for settings and support screen started

---
+++ packages/sshnp_gui/test/robots/settings_screen_robot.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sshnp_gui/src/controllers/config_controller.dart';
+import 'package:sshnp_gui/src/presentation/screens/settings_screen.dart';
+import 'package:sshnp_gui/src/presentation/widgets/contact_tile/contact_list_tile.dart';
+import 'package:sshnp_gui/src/presentation/widgets/custom_list_tile.dart';
+import 'package:sshnp_gui/src/utility/app_theme.dart';
+
+class SettingsScreenRobot {
+  SettingsScreenRobot(this.tester);
+
+  final WidgetTester tester;
+
+  Future<void> pumpSettingsScreen({ConfigListController? mockConfigListController}) async {
+    await tester.pumpWidget(ProviderScope(
+      child: MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: AppTheme.dark(),
+        home: const SettingsScreen(),
+      ),
+    ));
+  }
+
+  void findSettings() {
+    final finder = find.text('Settings');
+    expect(finder, findsOneWidget);
+  }
+
+  void findAccount() {
+    final finder = find.text('Account');
+    expect(finder, findsOneWidget);
+  }
+
+  void findContactListTile() {
+    final finder = find.byType(ContactListTile);
+    expect(finder, findsOneWidget);
+  }
+
+  void findSshKeyManagementListTile() {
+    final finder = find.widgetWithText(CustomListTile, 'SSH Key Management');
+    expect(finder, findsOneWidget);
+  }
+
+  void findBackupYourKeysListTile() {
+    final finder = find.widgetWithText(CustomListTile, 'Backup Your Keys');
+    expect(finder, findsOneWidget);
+  }
+
+  void findSettingsSwitchAtsignListTile() {
+    final finder = find.widgetWithText(CustomListTile, 'Switch atsign');
+    expect(finder, findsOneWidget);
+  }
+
+  void findSettingsResetAppActionButton() {
+    final finder = find.widgetWithText(CustomListTile, 'Reset App');
+    expect(finder, findsOneWidget);
+  }
+}
--- packages/sshnp_gui/test/src/presentation/screens/settings_screen_test.dart
+++ packages/sshnp_gui/test/src/presentation/screens/settings_screen_test.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../utility/settings_screen_robot.dart';
+import '../../../robots/settings_screen_robot.dart';
 
 void main() {
   testWidgets('Settings Screen widgets found', (widgetTester) async {
     final r = SettingsScreenRobot(widgetTester);
     await r.pumpSettingsScreen();
     r.findSettings();
-    r.findSettingBackupKeyActionButton();
-    r.findSettingsSwitchAtsignActionButton();
+    r.findAccount();
+    r.findContactListTile();
+    r.findSshKeyManagementListTile();
+    r.findBackupYourKeysListTile();
+    r.findSettingsSwitchAtsignListTile();
     r.findSettingsResetAppActionButton();
-    r.findSettingsFaqActionButton();
-    r.findSettingsContactAction();
-    r.findSettingsPrivacyPolicyAction();
     debugDumpApp();
   });
 }
