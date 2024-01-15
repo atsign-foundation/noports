fa7dcfec1bb2514076dcff25dfa06242754c0f7e
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-01-15 14:13:13 -0400

    fix: user see text update while screen is loading to ssh

--- packages/dart/sshnp_flutter/lib/src/presentation/widgets/profile_screen_widgets/profile_actions/profile_terminal_action.dart
+++ packages/dart/sshnp_flutter/lib/src/presentation/widgets/profile_screen_widgets/profile_actions/profile_terminal_action.dart
@@ -11,6 +11,7 @@ import 'package:sshnp_flutter/src/controllers/navigation_rail_controller.dart';
 import 'package:sshnp_flutter/src/controllers/terminal_session_controller.dart';
 import 'package:sshnp_flutter/src/presentation/widgets/profile_screen_widgets/profile_actions/profile_action_button.dart';
 import 'package:sshnp_flutter/src/repository/private_key_manager_repository.dart';
+import 'package:sshnp_flutter/src/utility/sizes.dart';
 
 import '../../../../repository/profile_private_key_manager_repository.dart';
 import '../../utility/custom_snack_bar.dart';
@@ -24,15 +25,25 @@ class ProfileTerminalAction extends ConsumerStatefulWidget {
 }
 
 class _ProfileTerminalActionState extends ConsumerState<ProfileTerminalAction> {
+  Future<void> showProgress(String status) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          gapH16,
+          Text(status),
+        ],
+      )),
+    );
+  }
+
   Future<void> onPressed() async {
-    log(widget.params.profileName ?? 'no profile name');
-    log(widget.params.clientAtSign ?? 'no client at sign');
     if (mounted) {
-      showDialog<void>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) => const Center(child: CircularProgressIndicator()),
-      );
+      showProgress('Starting Shell Session...');
     }
     // TODO: add try
     try {
@@ -64,7 +75,7 @@ class _ProfileTerminalActionState extends ConsumerState<ProfileTerminalAction> {
       final profilePrivateKey =
           await ProfilePrivateKeyManagerRepository.readProfilePrivateKeyManager(widget.params.profileName ?? '');
       final privateKeyManager =
-          await PrivateKeyManagerRepository.readPrivateKeyManager(profilePrivateKey?.privateKeyNickname ?? '');
+          await PrivateKeyManagerRepository.readPrivateKeyManager(profilePrivateKey.privateKeyNickname);
       // log('private key is: ${privateKeyManager!.privateKeyFileName}');
       // log('private key manager passphrase is: ${privateKeyManager.passPhrase}');
       // AtSshKeyPair keyPair = AtSshKeyPair.fromPem(
@@ -73,10 +84,7 @@ class _ProfileTerminalActionState extends ConsumerState<ProfileTerminalAction> {
       //   passphrase: privateKeyManager.passPhrase,
       //   // passphrase: privateKeyManager.passPhrase,
       // );
-      final keyPair = privateKeyManager?.toAtSshKeyPair();
-
-      log('key-id: ${keyPair?.identifier}');
-      log('private-key-populated: ${(keyPair?.privateKeyContents ?? '').isNotEmpty.toString()}');
+      final keyPair = privateKeyManager.toAtSshKeyPair();
 
       final sshnp = Sshnp.dartPure(
         // params: sshnpParams,
@@ -104,8 +112,17 @@ class _ProfileTerminalActionState extends ConsumerState<ProfileTerminalAction> {
 
       if (result is SshnpCommand) {
         if (sshnp.canRunShell) {
-          log('running shell session');
+          if (mounted) {
+            context.pop();
+            showProgress('running shell session');
+          }
+          log('running shell session...');
+
           SshnpRemoteProcess shell = await sshnp.runShell();
+          if (mounted) {
+            context.pop();
+            showProgress('starting terminal session...');
+          }
           log('starting terminal session');
           sessionController.startSession(
             shell,
@@ -114,7 +131,7 @@ class _ProfileTerminalActionState extends ConsumerState<ProfileTerminalAction> {
         }
 
         sessionController.issueDisplayName(widget.params.profileName!);
-        log('profile name is ${widget.params.profileName}');
+
         ref.read(navigationRailController.notifier).setRoute(AppRoute.terminal);
         if (mounted) {
           context.pushReplacementNamed(AppRoute.terminal.name);
