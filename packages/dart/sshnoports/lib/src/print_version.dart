4b8abfb7b75474bb8c72497ca81820f21f28689d
gkc <gary.casey@gmail.com>
2024-01-24 19:03:09 +0000

    fix: add missing `)` in print_version.dart

--- packages/dart/sshnoports/lib/src/print_version.dart
+++ packages/dart/sshnoports/lib/src/print_version.dart
@@ -5,5 +5,5 @@ import 'package:noports_core/version.dart' as package;
 
 /// Print version number
 void printVersion() {
-  stderr.writeln('Version : ${release.packageVersion} (core: ${package.packageVersion}');
+  stderr.writeln('Version : ${release.packageVersion} (core: ${package.packageVersion})');
 }
