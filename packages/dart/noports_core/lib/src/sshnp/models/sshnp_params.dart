c95f5e9f79599eea0f9df35c226ad1b61b41b072
gkc <gary.casey@gmail.com>
2024-02-28 13:24:22 +0000

    refactor: removing device.toLowerCase from various constructor initializers now that all of the relevant constructors are doing an `invalidDeviceName` check

--- packages/dart/noports_core/lib/src/sshnp/models/sshnp_params.dart
+++ packages/dart/noports_core/lib/src/sshnp/models/sshnp_params.dart
@@ -91,14 +91,14 @@ abstract class ClientParamsBase implements ClientParams {
     required this.sshnpdAtSign,
     required this.srvdAtSign,
     this.localPort = DefaultSshnpArgs.localPort,
-    String device = DefaultSshnpArgs.device,
+    this.device = DefaultSshnpArgs.device,
     this.verbose = DefaultArgs.verbose,
     this.atKeysFilePath,
     this.rootDomain = DefaultArgs.rootDomain,
     this.authenticateClientToRvd = DefaultArgs.authenticateClientToRvd,
     this.authenticateDeviceToRvd = DefaultArgs.authenticateDeviceToRvd,
     this.encryptRvdTraffic = DefaultArgs.encryptRvdTraffic,
-  }) : device = device.toLowerCase() {
+  }) {
     if (invalidDeviceName(device)) {
       throw ArgumentError(invalidDeviceNameMsg);
     }
