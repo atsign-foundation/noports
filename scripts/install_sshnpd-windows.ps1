7ab7868ae08ee6bc8e17d0c013e17d15c7f5a3d5
Xlin123 <xavier.li.1332@gmail.com>
2024-04-03 14:34:55 -0400

    feat: powershell script (check in)

---
+++ scripts/install_sshnpd-windows.ps1
@@ -0,0 +1,190 @@
+<#
+.SYNOPSIS
+   Installtion script for sshnpd on Windows
+.DESCRIPTION
+    Usage: install_sshnpd [options]
+
+    General options:
+      -u, --update                Update all services instead of installing
+          --rename                Rename device for client/device pair with the new name
+      -l, --local <path>          Install using local zip/tgz
+      -r, --repo <path>           Install using local repo
+      -h, --help                  Display this help message
+
+    Installation options:
+      -c, --client <address>      Client address (e.g. @alice_client)
+      -d, --device <address>      Device address (e.g. @alice_device)
+      -n, --name <device name>    Name of the device
+      -v, --version <version>     Version to install (default: latest)
+          --args <args>           Additional arguments to sshnpd ("-v" by default)
+          Possible args:
+            -s, --[no-]sshpublickey      Update authorized_keys to include public key from sshnp
+            -u, --[no-]un-hide           When set, makes various information visible to the manager atSign - e.g. username, version, etc
+            -v, --[no-]verbose           More logging
+
+    Rename options:
+      -c, --client <address>      Client address (e.g. @alice_client)
+      -n, --name <device name>    New name of the device
+.PARAMETER install_sshnpd OP
+    The operation to perform. Default is "install".
+    Rename options:
+      -c, --client <address>      Client address (e.g. @alice_client)
+      -n, --name <device name>    New name of the device
+.EXAMPLE
+    Usage: install_sshnpd [options]
+
+    General options:
+      -u, --update                Update all services instead of installing
+          --rename                Rename device for client/device pair with the new name
+      -l, --local <path>          Install using local zip/tgz
+      -r, --repo <path>           Install using local repo
+      -h, --help                  Display this help message
+
+    Installation options:
+      -c, --client <address>      Client address (e.g. @alice_client)
+      -d, --device <address>      Device address (e.g. @alice_device)
+      -n, --name <device name>    Name of the device
+      -v, --version <version>     Version to install (default: latest)
+          --args <args>           Additional arguments to sshnpd ("-v" by default)
+          Possible args:
+            -s, --[no-]sshpublickey      Update authorized_keys to include public key from sshnp
+            -u, --[no-]un-hide           When set, makes various information visible to the manager atSign - e.g. username, version, etc
+            -v, --[no-]verbose           More logging
+
+    Rename options:
+      -c, --client <address>      Client address (e.g. @alice_client)
+      -n, --name <device name>    New name of the device
+.NOTES
+    Author: [Xavier Lin]
+    Date:   [April 3, 2024]
+#>
+#Prints the help message via get-help install_sshnpd-windows.ps1 
+param(
+    [string]$SSHNP_OP = "install",
+    [switch]$SSHNP_CACHE_TEMP,
+    [string]$SSHNP_LOCAL,
+    [string]$SSHNP_DEV_MODE,
+    [Parameter(Mandatory=$true, HelpMessage="Specify the manager atsign.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$CLIENT_ATSIGN,
+    [string]$DEVICE_MANAGER_ATSIGN,
+    [Parameter(Mandatory=$true, HelpMessage="Specify the device atsign.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DEVICE_ATSIGN,
+    [string]$SSHNPD_DEVICE_NAME,
+    [string]$SSHNPD_VERSION,
+    [string]$SSHNPD_SERVICE_ARGS
+)
+
+
+# Set variables
+$BINARY_NAME = "sshnpd"
+
+# Define function to normalize atsign
+function Norm-Atsign {
+    param([string]$input)
+    $atsign = "@$($input -replace '"', '' -replace '^@', '')"
+    return $atsign
+}
+
+# Define function to normalize version
+function Norm-Version {
+    param([string]$input)
+    $version = "tags/v$($input -replace '"', '' -replace '^tags/', '' -replace '^v', '')"
+    return $version
+}
+
+# Define function to check basic requirements
+function Check-BasicRequirements {
+    $requiredCommands = @("ps", "Get-Process", "Select-String", "Select-Object", "Get-WmiObject", "StartService","Test-Path", "New-Item", "Get-Command", "New-Object", "Invoke-WebRequest", "New-Service")
+    
+    foreach ($command in $requiredCommands) {
+        if (-not (Get-Command -Name $command -ErrorAction SilentlyContinue)) {
+            Write-Host "[X] Missing required dependency: $command"
+        }
+    }
+}
+
+function Make-Dirs {
+    if (-not (Test-Path "$env:HOME\.atsign")) {
+        New-Item -Path "$env:HOME\.atsign" -ItemType Directory -Force
+    }
+    if(Test-Path "$env:HOME\.atsign\temp\$SSHNPD_VERSION") {
+        Remove-Item -Path "$env:HOME\.atsign\temp\$SSHNPD_VERSION" -Recurse -Force
+    }
+
+    
+    New-Item -Path "$env:HOME\.ssh\" -ItemType Directory -Force
+    New-Item -Path "$env:HOME\.$BINARY_NAME\logs" -ItemType Directory -Force
+    New-Item -Path "$env:HOME\.atsign\keys" -ItemType Directory -Force
+    New-Item -Path "$env:HOME\.atsign\temp\$SSHNPD_VERSION" -ItemType Directory -Force
+    New-Item -Path "$env:HOME\.local\bin" -ItemType Directory -Force
+
+    if (-not (Test-Path "$env:HOME\.ssh\authorized_keys" -PathType Leaf)) {
+        New-Item -Path "$env:HOME\.ssh\authorized_keys" -ItemType File -Force
+        Set-ItemProperty -Path "$env:HOME\.ssh\authorized_keys" -Name Mode -Value "600"
+    }
+}
+
+function Parse-Env {
+    $global:HOME_PATH = if (-not [string]::IsNullOrEmpty($env:HOME)) { $env:HOME } else { $env:USERPROFILE }
+    $global:SSHNPD_VERSION = if ([string]::IsNullOrEmpty($SSHNPD_VERSION)) { "latest" } else { Norm-Version $SSHNPD_VERSION }
+    $global:URL = "https://api.github.com/repos/atsign-foundation/noports/releases/$SSHNPD_VERSION"
+    $CLIENT_ATSIGN = Norm-Atsign $CLIENT_ATSIGN
+    $DEVICE_ATSIGN = Norm-Atsign $DEVICE_ATSIGN
+    $DEVICE_MANAGER_ATSIGN = Norm-Atsign $DEVICE_MANAGER_ATSIGN
+    $SSHNPD_VERSION =  Norm-Version $SSHNPD_VERSION
+}
+
+function Cleanup {
+    if (Test-Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION") {
+        Remove-Item -Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION" -Recurse -Force
+    }
+}
+
+function Download-Archive {
+    Write-Host "Downloading $BINARY_NAME from $global:URL"
+    Invoke-WebRequest -Uri $global:URL -OutFile "$global:HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME.zip"
+    if (-not (Test-Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME.zip")) {
+        Write-Host "Failed to download $BINARY_NAME"
+        Cleanup
+        Exit 1
+    }
+}
+
+function Unpack-Archive {
+    if (-not (Test-Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME.zip")) {
+        Write-Host "Failed to download $BINARY_NAME"
+        Exit 1
+    }
+
+    Expand-Archive -Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME.zip" -DestinationPath "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/" -Force
+    if (-not (Test-Path "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME")) {
+        Write-Host "Failed to unpack $BINARY_NAME"
+        Cleanup
+        Exit 1
+    }
+    $global:BIN_PATH = "$HOME_PATH/.atsign/temp/$SSHNPD_VERSION/$BINARY_NAME"
+}
+
+function New-SshnpdService {
+    New-Service -Name "sshnpd" -BinaryPathName "$global:BIN_PATH/sshnpd.exe" -DisplayName "sshnpd" -StartupType "Automatic"
+    Get-WmiObject -Class Win32_Service -Filter "Name='sshnpd'" | Start-Service
+}
+
+function Get-Main-Binaries {
+    Download-Archive
+    Unpack-Archive
+    New-SshnpdService
+}
+# Main function
+function Main {
+    # Check basic requirements
+    Check-BasicRequirements
+    Parse-Env
+    Make-Dirs
+    Get-Main-Binaries
+}
+
+# Execute the main function
+Main
