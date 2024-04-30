df9777a8bfb2e90c8ab681598418dc6a7bc78a68
xavierchanth <xchanthavong@gmail.com>
2024-04-30 17:35:12 -0400

    chore: better formatting for small message size

--- packages/go/trynoports/main.go
+++ packages/go/trynoports/main.go
@@ -25,19 +25,14 @@ const (
 	useHighPerformanceRenderer = false
 	welcomeMessageContent      = `Welcome to the NoPorts Trial environment!
 
-This environment allows you to test out NoPorts before doing device setup.
-If you're seeing this message, your client was set up correctly!
+This environment allows you to test out NoPorts before doing device setup. If you're seeing this message, your client was set up correctly!
 
 We have a few cool tricks you can try while you are here:
 
 #1 - List out all the network interfaces
 #2 - Run nmap to scan the public interface for open ports
 
-Combined, these will show you that there aren't any inbound ports open on this
-machine. We've taken a number of precautions to ensure that we minimize the
-network attack surface when you use NoPorts, while also making it as easy to
-use as possible. If you're curious about how the technology works, or you want
-to learn more about how we handle security, please visit our site:
+Combined, these will show you that there aren't any inbound ports open on this machine. We've taken a number of precautions to ensure that we minimize the network attack surface when you use NoPorts, while also making it as easy to use as possible. If you're curious about how the technology works, or you want to learn more about how we handle security, please visit our site:
 
 https://www.noports.com/sshnp-how-it-works
 
