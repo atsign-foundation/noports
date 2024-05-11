1b719ead9f9bfec2a4b79029913ac74ce0c5bac5
xavierchanth <xchanthavong@gmail.com>
2024-05-10 20:17:20 -0400

    feat: ping request working

--- packages/c/sshnpd/src/main.c
+++ packages/c/sshnpd/src/main.c
@@ -274,8 +274,6 @@ int main(int argc, char **argv) {
           }
         }
 
-        printf("pass\n");
-        printf("key: %s\n", key);
         atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Received key: %s\n", key);
         switch (notification_key) {
         case NK_SSHPUBLICKEY: {
@@ -283,26 +281,27 @@ int main(int argc, char **argv) {
           break;
         }
         case NK_PING: {
-          atclient_atkey *pingkey;
-          atclient_atkey_init(pingkey);
+          atclient_atkey pingkey;
+          atclient_atkey_init(&pingkey);
 
-          size_t keynamelen = strlen("heartbeat") + strlen(params.device) + 1; // + 1 for '.'
-          char keyname[keynamelen + 1];                                        // +1 for '\0'
+          size_t keynamelen = strlen("heartbeat") + strlen(params.device) + 2; // + 1 for '.' +1 for '\0'
+          char keyname[keynamelen];
           snprintf(keyname, keynamelen, "heartbeat.%s", params.device);
-          atclient_atkey_create_sharedkey(pingkey, keyname, keynamelen, params.atsign, strlen(params.atsign),
+          printf("keyname: %s\n", keyname);
+          atclient_atkey_create_sharedkey(&pingkey, keyname, keynamelen, params.atsign, strlen(params.atsign),
                                           message->notification.from, strlen(message->notification.from), SSHNP_NS,
                                           SSHNP_NS_LEN);
 
-          atclient_atkey_metadata *metadata = &pingkey->metadata;
+          atclient_atkey_metadata *metadata = &pingkey.metadata;
           atclient_atkey_metadata_set_ispublic(metadata, false);
           atclient_atkey_metadata_set_isencrypted(metadata, true);
           atclient_atkey_metadata_set_ttl(metadata, 10000);
 
-          atclient_notify_params *notify_params;
-          atclient_notify_params_init(notify_params);
-          notify_params->key = *pingkey;
-          notify_params->value = ping_response;
-          notify_params->operation = ATCLIENT_NOTIFY_OPERATION_UPDATE;
+          atclient_notify_params notify_params;
+          atclient_notify_params_init(&notify_params);
+          notify_params.key = pingkey;
+          notify_params.value = ping_response;
+          notify_params.operation = ATCLIENT_NOTIFY_OPERATION_UPDATE;
 
           int ret = pthread_mutex_lock(&atclient_lock);
           if (ret != 0) {
@@ -311,7 +310,7 @@ int main(int argc, char **argv) {
             goto exit_ping;
           }
 
-          ret = atclient_notify(&atclient, notify_params, NULL);
+          ret = atclient_notify(&atclient, &notify_params, NULL);
           if (ret != 0) {
             atlogger_log("PING RESPONSE", ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to send ping response to %s\n",
                          message->notification.from);
@@ -326,8 +325,8 @@ int main(int argc, char **argv) {
           } while (ret != 0);
           atlogger_log("PING RESPONSE", ATLOGGER_LOGGING_LEVEL_DEBUG, "Released the atclient lock\n");
         exit_ping:
-          atclient_notify_params_free(notify_params);
-          atclient_atkey_free(pingkey);
+          atclient_notify_params_free(&notify_params);
+          atclient_atkey_free(&pingkey);
           break;
         }
         case NK_SSH_REQUEST: {
