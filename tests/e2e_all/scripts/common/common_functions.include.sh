7febd36d781dddb501351c6b3c0d8d0918b7bccc
gkc <gary.casey@gmail.com>
2024-05-30 18:16:50 +0100

    test: fix: ensure the cmake build dir exists

--- tests/e2e_all/scripts/common/common_functions.include.sh
+++ tests/e2e_all/scripts/common/common_functions.include.sh
@@ -539,6 +539,7 @@ buildCurrentCBinaries() {
     logInfo "        Compiling sshnpd"
     local base_dir="./sshnpd"
     local build_dir="$base_dir/build"
+    mkdir -p "$build_dir"
     cmake -B $build_dir -S $base_dir
     cmake --build $build_dir
     cp $build_dir/sshnpd "$binaryOutputDir/"
