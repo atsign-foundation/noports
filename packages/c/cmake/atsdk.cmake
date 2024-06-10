dd762935c6909b4d9c9fb3eb7b6ac8284f2e020d
xavierchanth <xchanthavong@gmail.com>
2024-06-10 19:29:19 +0000

    use latest atsdk

--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -3,7 +3,7 @@ if(NOT atsdk_FOUND)
   FetchContent_Declare(
     atsdk
     GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG d961be6633b88d6f8c1d9906d783b3ad95b8fdb1
+    GIT_TAG b6be628afeb58fc829019b6a5e032ee42d1efe53
   )
   FetchContent_MakeAvailable(atsdk)
   install(TARGETS atclient atchops atlogger)
