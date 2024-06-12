748d0e33463ae3ec23ff9b0821d21627e9a64193
realvarx <alvaromerarg@gmail.com>
2024-06-13 01:50:09 +0200

    feat: verify_envelope_signature method working

--- packages/c/sshnpd/include/sshnpd/handle_ssh_request.h
+++ packages/c/sshnpd/include/sshnpd/handle_ssh_request.h
@@ -7,4 +7,8 @@
 void handle_ssh_request(atclient *atclient, pthread_mutex_t *atclient_lock, sshnpd_params *params,
                         atclient_monitor_message *message, char *home_dir, FILE *authkeys_file, char *authkeys_filename,
                         atchops_rsakey_privatekey signing_key);
+
+int verify_envelope_signature(atchops_rsakey_publickey publickey, const unsigned char *payload,
+                              unsigned char *signature, const char *hashing_algo, const char *signing_algo);
+
 #endif
