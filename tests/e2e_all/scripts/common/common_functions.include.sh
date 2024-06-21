6e57974d975773bf32d50521b58525796e4b49d3
xavierchanth <xchanthavong@gmail.com>
2024-06-21 21:53:27 +0000

    ci: lessen the strictness, but enable -Werror-implicit-function-declaration

--- tests/e2e_all/scripts/common/common_functions.include.sh
+++ tests/e2e_all/scripts/common/common_functions.include.sh
@@ -540,7 +540,7 @@ buildCurrentCBinaries() {
     local base_dir="./sshnpd"
     local build_dir="$base_dir/build"
     mkdir -p "$build_dir"
-    cmake -B $build_dir -S $base_dir -DCMAKE_C_COMPILER=gcc -DCMAKE_C_FLAGS="-Wall -Wextra -Wpedantic -Werror" -DBUILD_TESTS=off
+    cmake -B $build_dir -S $base_dir -DCMAKE_C_COMPILER=gcc -DCMAKE_C_FLAGS="-Wall -Wextra -Werror-implicit-function-declaration" -DBUILD_TESTS=off
     cmake --build $build_dir
     if [ $? -ne 0 ]; then
       logErrorAndExit "cmake build failed"
