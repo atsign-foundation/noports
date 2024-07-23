0d67c655460ab7b43516b7acece72616ad5c3b89
gkc <gary.casey@gmail.com>
2024-07-23 14:31:30 +0100

    feat: some normalization of the yaml structure by adding userGroups

--- packages/dart/sshnoports/bin/npa_file.dart
+++ packages/dart/sshnoports/bin/npa_file.dart
@@ -44,28 +44,92 @@ void main(List<String> args) async {
 class FileBasedPolicy implements NPARequestHandler {
   YamlMap yaml;
 
-  final Set<String> _daemonAtsigns = {};
+  final Set<String> _daemonAtSigns = {};
+  final Map<String, Map> _userAtSigns = {};
 
   FileBasedPolicy(this.yaml) {
-    // iterate through the map, gather all of the daemon atSigns
-    // top level keys are the client atSigns
-    for (String clientAtsign in yaml.keys) {
-      // Next level is a map of daemonAtsigns
-      for (String daemonAtsign in yaml[clientAtsign].keys) {
-        _daemonAtsigns.add(daemonAtsign);
+    // Get the full list of daemonAtSigns which this policy service will listen to
+    for (String userGroupName in yaml['userGroups'].keys ?? []) {
+      for (String daemonAtSign in yaml['userGroups'][userGroupName]
+              ['permissions']['daemonAtSigns'] ??
+          []) {
+        _daemonAtSigns.add(daemonAtSign);
+      }
+    }
+    print(_daemonAtSigns);
+
+    // Create a map of userAtSign->daemonAtSign->deviceNames/deviceGroupNames->[PermitOpens]
+    for (String userGroupName in yaml['userGroups'].keys ?? []) {
+      final group = yaml['userGroups'][userGroupName];
+      for (String userAtSign in group['userAtSigns']) {
+        _userAtSigns.putIfAbsent(
+            userAtSign, () => {'userGroups': [], 'daemons': {}});
+        ((_userAtSigns[userAtSign] as Map)['userGroups'] as List)
+            .add(userGroupName);
+        Map userDaemonPermissions =
+            (_userAtSigns[userAtSign] as Map)['daemons'] as Map;
+        for (String daemonAtSign
+            in group['permissions']['daemonAtSigns'] ?? []) {
+          userDaemonPermissions.putIfAbsent(
+              daemonAtSign, () => {'deviceNames': {}, 'deviceGroupNames': {}});
+
+          for (String deviceName
+              in (group['permissions']['deviceNames'] ?? {}).keys) {
+            Map devicesMap =
+                userDaemonPermissions[daemonAtSign]['deviceNames'] as Map;
+            devicesMap.putIfAbsent(deviceName, () => []);
+            final devicePermissions = devicesMap[deviceName] as List;
+            for (String permitOpen in List<String>.from(
+                group['permissions']['deviceNames'][deviceName] ?? [])) {
+              if (!devicePermissions.contains(permitOpen)) {
+                devicePermissions.add(permitOpen);
+              }
+            }
+          }
+
+          for (String deviceGroupName
+              in (group['permissions']['deviceGroupNames'] ?? {}).keys) {
+            Map deviceGroupsMap =
+                userDaemonPermissions[daemonAtSign]['deviceGroupNames'] as Map;
+            deviceGroupsMap.putIfAbsent(deviceGroupName, () => []);
+            final deviceGroupPermissions =
+                deviceGroupsMap[deviceGroupName] as List;
+            for (String permitOpen in List<String>.from(group['permissions']
+                    ['deviceGroupNames'][deviceGroupName] ??
+                [])) {
+              if (!deviceGroupPermissions.contains(permitOpen)) {
+                deviceGroupPermissions.add(permitOpen);
+              }
+            }
+          }
+        }
+      }
+    }
+    for (final u in _userAtSigns.keys) {
+      final user = _userAtSigns[u] as Map;
+      print('$u is member of groups ${user['userGroups']}');
+      for (final d in user['daemons'].keys) {
+        print('  daemon: $d');
+        var daemon = user['daemons'][d];
+        for (final dn in (daemon['deviceNames']).keys) {
+          print('    device $dn: ${daemon['deviceNames'][dn]}');
+        }
+        for (final dgn in (daemon['deviceGroupNames']).keys) {
+          print('    deviceGroup $dgn: ${daemon['deviceGroupNames'][dgn]}');
+        }
       }
     }
   }
 
-  Set<String> get daemonAtsigns => _daemonAtsigns;
+  Set<String> get daemonAtsigns => _daemonAtSigns;
 
   @override
   Future<NPAAuthCheckResponse> doAuthCheck(
       NPAAuthCheckRequest authCheckRequest) async {
     /// - The policy service needs to check if the clientAtsign is
     ///   - 1. permitted to talk to this daemonAtsign
-    ///     - --> is there a value at permissions[@client][@daemon]
-    final clientEntry = yaml[authCheckRequest.clientAtsign];
+    ///     - --> is there a value at _userAtSigns[@client][@daemon]
+    final clientEntry = _userAtSigns[authCheckRequest.clientAtsign];
     if (clientEntry == null) {
       return NPAAuthCheckResponse(
         authorized: false,
@@ -73,7 +137,7 @@ class FileBasedPolicy implements NPARequestHandler {
         permitOpen: [],
       );
     }
-    final daemonEntry = clientEntry[authCheckRequest.daemonAtsign];
+    final daemonEntry = clientEntry['daemons'][authCheckRequest.daemonAtsign];
     if (daemonEntry == null) {
       return NPAAuthCheckResponse(
         authorized: false,
