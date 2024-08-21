1604e25e23c50725894540c4920c980f042c2c40
xavierchanth <xchanthavong@gmail.com>
2024-08-20 22:22:55 -0400

    Revert "feat: prevent quit on close, hidden at launch"
    This reverts commit 9134e3f1d5d01911f396a13e42418a5cf99785e3.

--- packages/dart/npt_flutter/macos/Runner/AppDelegate.swift
+++ packages/dart/npt_flutter/macos/Runner/AppDelegate.swift
@@ -4,6 +4,6 @@ import FlutterMacOS
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return false
+    return true
   }
 }
