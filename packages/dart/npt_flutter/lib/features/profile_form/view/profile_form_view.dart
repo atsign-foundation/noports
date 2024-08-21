7fff7777873c9b81893a94f5d10783d7404e7962
xavierchanth <xchanthavong@gmail.com>
2024-08-21 14:44:01 -0400

    fix: load global state for profile form

--- packages/dart/npt_flutter/lib/features/profile_form/view/profile_form_view.dart
+++ packages/dart/npt_flutter/lib/features/profile_form/view/profile_form_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:npt_flutter/features/profile/profile.dart';
 import 'package:npt_flutter/features/profile_form/profile_form.dart';
+import 'package:npt_flutter/widgets/spinner.dart';
 
 class ProfileFormView extends StatelessWidget {
   final String uuid;
@@ -12,41 +13,55 @@ class ProfileFormView extends StatelessWidget {
     return BlocProvider<ProfileBloc>(
       create: (BuildContext context) =>
 
-          /// Don't use [ProfileCacheCubit] here, if we don't hit submit we want
-          /// all of the edits to automatically be lost
-          ProfileBloc(context.read<ProfileRepository>(), uuid)
+          /// Global copy of the profile which is shared with the rest of the app
+          context.read<ProfileCacheCubit>().getProfileBloc(uuid)
             ..add(const ProfileLoadOrCreateEvent()),
-      child: Column(
-        children: [
-          const ProfileDisplayNameTextField(),
-          const ProfileDeviceAtSignTextField(),
-          const ProfileDeviceNameTextField(),
-          const ProfileRelayAtSignTextField(),
-          const ProfileRelayQuickButtons(),
-          const ProfileRemoteHostTextField(),
-          const ProfileRemotePortSelector(),
-          const ProfileLocalPortSelector(),
-          Builder(
-            builder: (context) => ElevatedButton(
-              onPressed: () {
-                var localBloc = context.read<ProfileBloc>();
-                var globalBloc =
-                    context.read<ProfileCacheCubit>().getProfileBloc(uuid);
-                if (localBloc.state is ProfileLoadedState &&
-                    globalBloc.state is ProfileLoadedState) {
-                  globalBloc.add(ProfileEditEvent(
-                    profile: (localBloc.state as ProfileLoadedState).profile,
-                    save: true,
-                    addToProfilesList: true,
-                    popNavAfterAddToProfilesList: true,
-                  ));
-                }
-              },
-              child: const Text("Submit"),
-            ),
-          ),
-        ],
-      ),
+      child: BlocSelector<ProfileBloc, ProfileState, bool>(
+          selector: (state) => state is ProfileLoadedState,
+          builder: (context, isLoaded) {
+            if (!isLoaded) return const Spinner();
+            return BlocProvider<ProfileBloc>(
+              create: (BuildContext context) =>
+
+                  /// Local copy of the profile which is used by the form
+                  ProfileBloc(context.read<ProfileRepository>(), uuid)
+                    ..add(const ProfileLoadOrCreateEvent()),
+              child: Column(
+                children: [
+                  const ProfileDisplayNameTextField(),
+                  const ProfileDeviceAtSignTextField(),
+                  const ProfileDeviceNameTextField(),
+                  const ProfileRelayAtSignTextField(),
+                  const ProfileRelayQuickButtons(),
+                  const ProfileRemoteHostTextField(),
+                  const ProfileRemotePortSelector(),
+                  const ProfileLocalPortSelector(),
+                  Builder(
+                    builder: (context) => ElevatedButton(
+                      onPressed: () {
+                        var localBloc = context.read<ProfileBloc>();
+                        var globalBloc = context
+                            .read<ProfileCacheCubit>()
+                            .getProfileBloc(uuid);
+
+                        if (localBloc.state is ProfileLoadedState &&
+                            globalBloc.state is ProfileLoadedState) {
+                          globalBloc.add(ProfileEditEvent(
+                            profile:
+                                (localBloc.state as ProfileLoadedState).profile,
+                            save: true,
+                            addToProfilesList: true,
+                            popNavAfterAddToProfilesList: true,
+                          ));
+                        }
+                      },
+                      child: const Text("Submit"),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
