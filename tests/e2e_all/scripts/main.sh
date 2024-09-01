3d943e91d692e9f926a064578cb45b1a0ac678d9
gkc <gary.casey@gmail.com>
2024-09-01 18:33:49 +0100

    test: re-adding the C daemon to the e2e test matrix, but leaving test parallelization disabled

--- tests/e2e_all/scripts/main.sh
+++ tests/e2e_all/scripts/main.sh
@@ -49,7 +49,7 @@ atDirectoryPort=64
 testsToRun="all"
 
 # defaultDaemonVersions="c:current"
-defaultDaemonVersions="d:4.0.5 d:5.2.0 d:5.5.0 d:current"
+defaultDaemonVersions="d:4.0.5 d:5.2.0 d:5.5.0 d:current c:current"
 defaultClientVersions="d:4.0.5 d:5.2.0 d:5.5.0 d:current"
 
 daemonVersions=$defaultDaemonVersions
