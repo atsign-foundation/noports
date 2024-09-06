72968005dda4ab51aef8b1f62255c11928b5518e
xavierchanth <xchanthavong@gmail.com>
2024-09-05 20:01:30 -0400

    chore: add todo for later

--- tools/demo-env/policy/bootstrap.sh
+++ tools/demo-env/policy/bootstrap.sh
@@ -4,6 +4,9 @@ create_api_group() {
     --header='Content-Type:application/json' \
     "http://$HOST:$PORT/api/policy/group"
 }
+
+# TODO: Multiple bob/alice devices, with different groups
+# roles around groups
 if [ -f /second_run ]; then
   create_api_group '{"name":"Barbara","description":"Full access to alice","daemonAtSigns":["@alice🛠"],"devices":[{"name":"default","permitOpens":["localhost:*"]}],"deviceGroups":[],"userAtSigns":["@barbara🛠"]}'
   create_api_group '{"name":"Murali","description":"Remote desktop access only","daemonAtSigns":["@bob🛠"],"devices":[{"name":"default","permitOpens":["*:5900","*:3389"]}],"deviceGroups":[{"name":"device group name","permitOpens":[]}],"userAtSigns":["@murali🛠"]}'
