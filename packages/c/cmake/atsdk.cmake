41b39cf9a0f192deec7261ef67cc32a1c763b658
Chris Swan <478926+cpswan@users.noreply.github.com>
2024-09-11 13:21:05 +0100

    build(deps): Use v0.2.0 of at_c

--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -3,7 +3,7 @@ if(NOT atsdk_FOUND)
   FetchContent_Declare(
     atsdk
     GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG dc9fc44e789b91887a5be13c433d6039b72aaea6
+    GIT_TAG 0b0be7bfcf5765819156b55a9a81aaa3b30945bf
   )
   FetchContent_MakeAvailable(atsdk)
   install(TARGETS atclient atchops atlogger)
