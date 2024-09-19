36060f80800d998646cc4619a769b1ff32ce2349
xavierchanth <xchanthavong@gmail.com>
2024-09-19 19:58:59 -0400

    fix: use atsdk with metadata fix

--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -3,7 +3,7 @@ if(NOT atsdk_FOUND)
   FetchContent_Declare(
     atsdk
     GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG 75d60d810ec26e02fa8e29e674626551325d1bf8
+    GIT_TAG 22340564c40831f0647ce434616da5dd0d07813a
   )
   FetchContent_MakeAvailable(atsdk)
   install(TARGETS atclient atchops atlogger)
