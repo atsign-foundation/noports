90e8a273d241a1cf5749d2fb27e37ccb40fcd19f
xavierchanth <xchanthavong@gmail.com>
2024-09-20 13:13:53 -0400

    chore: uptake at_c commit

--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -3,7 +3,7 @@ if(NOT atsdk_FOUND)
   FetchContent_Declare(
     atsdk
     GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG 5760df6e89b402fa29a800971002dbd5ff2fe607
+    GIT_TAG 22502d8d67469d8659baf58c7d946875e114877a
   )
   FetchContent_MakeAvailable(atsdk)
   install(TARGETS atclient atchops atlogger)
