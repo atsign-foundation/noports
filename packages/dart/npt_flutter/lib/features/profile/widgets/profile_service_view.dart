39ff8b79ca1401ffa8e9a53a1896915a3addb83a
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-09-26 09:58:14 -0400

    fix: profile, device name, service mapping and status updated to support scrolling.

--- packages/dart/npt_flutter/lib/features/profile/widgets/profile_service_view.dart
+++ packages/dart/npt_flutter/lib/features/profile/widgets/profile_service_view.dart
@@ -11,14 +11,17 @@ class ProfileServiceView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: Sizes.p150,
-      child: BlocSelector<ProfileBloc, ProfileState, (int, String, int)?>(selector: (state) {
-        if (state is! ProfileLoadedState) return null;
-        return (state.profile.localPort, state.profile.remoteHost, state.profile.remotePort);
-      }, builder: (BuildContext context, (int, String, int)? triple) {
-        if (triple == null) return gap0;
-        var (localPort, remoteHost, remotePort) = triple;
-        return Text('$localPort:$remoteHost:$remotePort');
-      }),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: BlocSelector<ProfileBloc, ProfileState, (int, String, int)?>(selector: (state) {
+          if (state is! ProfileLoadedState) return null;
+          return (state.profile.localPort, state.profile.remoteHost, state.profile.remotePort);
+        }, builder: (BuildContext context, (int, String, int)? triple) {
+          if (triple == null) return gap0;
+          var (localPort, remoteHost, remotePort) = triple;
+          return Text('$localPort:$remoteHost:$remotePort');
+        }),
+      ),
     );
   }
 }
