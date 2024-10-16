6aa2e36d847eff1ca21f8416c671556fab9d8543
xavierchanth <xchanthavong@gmail.com>
2024-10-16 16:30:03 -0400

    chore: cleanup window manager

--- packages/dart/npt_flutter/lib/main.dart
+++ packages/dart/npt_flutter/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -8,11 +6,10 @@ import 'app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   windowManager.ensureInitialized();
-  try {
-    await windowManager.setSkipTaskbar(true); // Don't show the app icon in dock
-  } catch (_) {
-    log("Failed to setSkipTaskbar");
-  } finally {
-    runApp(const App());
-  }
+  var windowOptions = const WindowOptions(
+    title: "NoPorts Desktop",
+    skipTaskbar: true,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions);
+  runApp(const App());
 }
