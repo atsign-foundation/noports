59c8f1edfe0e662f827557f46d9026babf54a4fa
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-10-24 09:03:48 -0400

    fix: updated windowManager options.

--- packages/dart/npt_flutter/lib/main.dart
+++ packages/dart/npt_flutter/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:npt_flutter/constants.dart';
 import 'package:window_manager/window_manager.dart';
@@ -8,17 +6,13 @@ import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows) {
-    final wm = WindowManager.instance;
-    await wm.ensureInitialized();
-    await wm.setMinimumSize(Constants.kWindowsMinWindowSize);
-  }
 
   var windowOptions = const WindowOptions(
     title: "NoPorts Desktop",
+    minimumSize: Constants.kWindowsMinWindowSize,
     skipTaskbar: true,
   );
-
+  windowManager.ensureInitialized();
   windowManager.waitUntilReadyToShow(windowOptions);
   runApp(const App());
 }
