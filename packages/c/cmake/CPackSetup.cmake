100eed429c10286e91b359edaa8f56d21d3fcccb
XavierChanth <xchanthavong@gmail.com>
2025-03-19 15:36:41 -0400

    fix: use cpack for c_release source tarballs

---
+++ packages/c/cmake/CPackSetup.cmake
@@ -0,0 +1,56 @@
+# package info
+set(CPACK_PACKAGE_NAME noports)
+set(CPACK_PACKAGE_DESCRIPTION "noports source tarballs")
+set(CPACK_PACKAGE_VERSION 1.0.3)
+set(CPACK_PACKAGE_VENDOR_NAME atsign-foundation)
+
+# cmake configuration
+set(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+
+# static cpack configuration
+set(CPACK_PACKAGE_FILE_NAME noports-static)
+set(CPACK_GENERATOR ZIP TGZ)
+set(CPACK_INSTALL_CMAKE_PROJECTS ".;noports;ALL;/")
+
+# source cpack configuration
+set(CPACK_SOURCE_PACKAGE_FILE_NAME noports-source)
+set(CPACK_SOURCE_GENERATOR TGZ ZIP)
+
+set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_CURRENT_SOURCE_DIR};/")
+
+# Yes, you need 4 * '\' to escape literal '.'
+set(
+  CPACK_SOURCE_IGNORE_FILES
+  # directories
+  "/\\\\.cache/"
+  "/\\\\.git/"
+  "/\\\\.github/"
+  "/\\\\.jj/"
+  "/\\\\.vscode/"
+  "/CMakeFiles/"
+  "/docs/"
+  "/build/"
+  "/logs/"
+  "/graceful-shutdown-tool/"
+  "/valgrind-test-scenarios.md"
+  # root files
+  "/\\\\.clang-format"
+  "/\\\\.clang-tidy"
+  "/\\\\.clangd"
+  "/just.env"
+  "/just.template.env"
+  "/justfile"
+  # (Potentially) nested files
+  "\\\\.DS_Store"
+  "\\\\.gitignore"
+  "compile_commands.json"
+)
+
+set(CPACK_INSTALL_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/CPackSourceDeps.cmake")
+configure_file(
+  "${CMAKE_SOURCE_DIR}/cmake/CPackSourceDeps.cmake"
+  "CPackSourceDeps.cmake"
+  @ONLY
+)
+
+include(CPack)
--- packages/c/cmake/atsdk.cmake
+++ packages/c/cmake/atsdk.cmake
@@ -1,14 +1,22 @@
+option(NOPORTS_ATSDK_PATH "Local atsdk path" OFF)
 if(NOT atsdk_FOUND)
-  message(STATUS "atsdk not found, fetching from GitHub..")
-  FetchContent_Declare(
-    atsdk
-    GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
-    GIT_TAG b542e87d0d90f387b326555f65527752e229e821
-  )
+  message(STATUS "[atsdk] fetching package...")
+  include(FetchContent)
+  if(NOPORTS_ATSDK_PATH)
+    FetchContent_Declare(
+      atsdk
+      SOURCE_DIR
+      ${CMAKE_SOURCE_DIR}/${NOPORTS_ATSDK_PATH}
+    )
+  else()
+    FetchContent_Declare(
+      atsdk
+      GIT_REPOSITORY https://github.com/atsign-foundation/at_c.git
+      GIT_TAG b542e87d0d90f387b326555f65527752e229e821
+    )
+  endif()
   FetchContent_MakeAvailable(atsdk)
   install(
     TARGETS atclient atchops atlogger atauth atcommons
   )
-else()
-  message(STATUS "atsdk already installed...")
 endif()
