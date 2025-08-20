da8220a2f05306e347118f886df82c70f3ac6ab4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-08-19 22:11:18 -0400

    fix:

--- packages/dart/npt_flutter/lib/widgets/switch_atsign_button.dart
+++ packages/dart/npt_flutter/lib/widgets/switch_atsign_button.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
