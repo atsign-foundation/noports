758cff2aae3f39cbd1c61daa779c9ff4012694c2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-08-25 08:53:31 -0400

    refactor: name of folder

--- packages/dart/npt_flutter/lib/features/policy_form/widgets/form_content.dart
+++ packages/dart/npt_flutter/lib/features/policy_form/widgets/form_content.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../policy_manager/models/policy.dart';
-import '../../policy_manager/cubit/policy_manager_cubit.dart';
+import '../../policy/models/policy.dart';
+import '../../policy/cubit/policy_manager_cubit.dart';
 import 'role_name_field.dart';
 import 'role_description_field.dart';
 import 'daemon_at_signs_field.dart';
