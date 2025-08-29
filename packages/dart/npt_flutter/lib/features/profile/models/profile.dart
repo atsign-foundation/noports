ace765b5914145f0b4af28f3d42b2ec9678be4ed
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-08-29 12:53:56 -0400

    fix: add @jsonKey on top of keepAlive

--- packages/dart/npt_flutter/lib/features/profile/models/profile.dart
+++ packages/dart/npt_flutter/lib/features/profile/models/profile.dart
@@ -19,6 +19,7 @@ final class Profile extends Loggable with Favoritable {
   final int remotePort;
   final int localPort;
   final bool only443;
+  @JsonKey(defaultValue: true)
   final bool keepAlive;
 
   const Profile(
