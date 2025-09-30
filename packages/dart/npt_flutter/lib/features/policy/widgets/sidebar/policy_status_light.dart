a0acecacb71083b737777a06459fa5595997ff5b
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-09-29 23:24:37 -0400

    feat(widget): policy_status_light.dart

---
+++ packages/dart/npt_flutter/lib/features/policy/widgets/sidebar/policy_status_light.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../styles/app_color.dart';
+import '../../cubit/status_light/policy_status_light_cubit.dart';
+import '../../cubit/status_light/policy_status_light_state.dart';
+import 'status_light.dart';
+
+class PolicyStatusLight extends StatelessWidget {
+  const PolicyStatusLight({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PolicyStatusLightCubit, PolicyStatusLightState>(
+      builder: (context, state) {
+        final _StatusIndicatorData data = _resolveStateData(state);
+
+        return Tooltip(
+          message: data.tooltip,
+          waitDuration: const Duration(milliseconds: 400),
+          child: Semantics(
+            label: data.tooltip,
+            child: StatusLight(color: data.color),
+          ),
+        );
+      },
+    );
+  }
+
+  _StatusIndicatorData _resolveStateData(PolicyStatusLightState state) {
+    if (state is PolicyStatusLightLoaded) {
+      return _StatusIndicatorData(
+        color: state.lightState == LightState.green
+            ? AppColor.successColor
+            : AppColor.errorColor,
+        tooltip: state.message ?? _defaultMessage(state.lightState),
+      );
+    }
+
+    return const _StatusIndicatorData(
+      color: AppColor.greyColor,
+      tooltip: 'Loading heartbeat status…',
+    );
+  }
+
+  String _defaultMessage(LightState lightState) {
+    return switch (lightState) {
+      LightState.green => 'Heartbeat healthy',
+      LightState.red => 'Heartbeat unavailable',
+    };
+  }
+}
+
+class _StatusIndicatorData {
+  const _StatusIndicatorData({required this.color, required this.tooltip});
+
+  final Color color;
+  final String tooltip;
+}
