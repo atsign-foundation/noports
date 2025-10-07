21ee4680436c2cb807de2a4cc214872f5a7756bd
gkc <gary.casey@gmail.com>
2025-10-07 12:15:32 +0100

    [skip ci] chore: put files back the way they were

--- packages/dart/noports_core/lib/src/common/io_types.dart
+++ packages/dart/noports_core/lib/src/common/io_types.dart
@@ -1,7 +1,5 @@
 /// This file contains all of the dart:io calls in noports_core
 /// All io used should be wrapped for the sake of testing and compatibility
-library io_types;
-
 import 'dart:io' show Process, ProcessResult, ProcessStartMode;
 import 'package:meta/meta.dart';
 
@@ -11,18 +9,16 @@ export 'package:file/file.dart';
 export 'package:file/local.dart' show LocalFileSystem;
 
 @internal
-typedef ProcessRunner =
-    Future<ProcessResult> Function(
-      String executable,
-      List<String> arguments, {
-      String? workingDirectory,
-    });
+typedef ProcessRunner = Future<ProcessResult> Function(
+  String executable,
+  List<String> arguments, {
+  String? workingDirectory,
+});
 
 @internal
-typedef ProcessStarter =
-    Future<Process> Function(
-      String executable,
-      List<String> arguments, {
-      bool runInShell,
-      ProcessStartMode mode,
-    });
+typedef ProcessStarter = Future<Process> Function(
+  String executable,
+  List<String> arguments, {
+  bool runInShell,
+  ProcessStartMode mode,
+});
