1b61ae85210114ad6a6725aa3b4533e03d61f847
gkc <gary.casey@gmail.com>
2025-10-07 12:13:46 +0100

    [skip ci] chore: put files back the way they were

--- packages/dart/noports_core/lib/src/common/handle_server_events.dart
+++ packages/dart/noports_core/lib/src/common/handle_server_events.dart
@@ -11,71 +11,71 @@ handlePublicKeyChangedEvent(AtClient atClient, Atsign atSign) {
   atClient.notificationService
       .subscribe(regex: topic, shouldDecrypt: true)
       .listen((AtNotification n) async {
-        final dynamic ej;
-        try {
-          ej = jsonDecode(n.value!);
-        } catch (e) {
-          logger.shout(
-            'Caught exception $e'
-            ' while handling server event notification $n',
-          );
-          return;
-        }
+    final dynamic ej;
+    try {
+      ej = jsonDecode(n.value!);
+    } catch (e) {
+      logger.shout(
+        'Caught exception $e'
+        ' while handling server event notification $n',
+      );
+      return;
+    }
 
-        final category = ej['category'];
-        if (category == null) {
-          logger.shout('No "category" in server event $ej');
-          return;
-        }
-        final name = ej['name'];
-        if (name == null) {
-          logger.shout('No "name" in server event $ej');
-          return;
-        }
+    final category = ej['category'];
+    if (category == null) {
+      logger.shout('No "category" in server event $ej');
+      return;
+    }
+    final name = ej['name'];
+    if (name == null) {
+      logger.shout('No "name" in server event $ej');
+      return;
+    }
 
-        try {
-          switch (category) {
-            case AtServerEvent.atProtocolCategory:
-              final name = ej['name'];
-              switch (name) {
-                case AtServerEvent.atSignPKChangedEventName:
-                  logger.shout('HANDLING public key change event: $ej');
-                  AtSignPKChangedEvent e = AtSignPKChangedEvent.fromJson(ej);
-                  List<String> keysToRemove = [
-                    'shared_key.${e.atSign.substring(1)}$atSign',
-                    '${e.atSign}:shared_key$atSign',
-                    'cached:public:publickey${e.atSign}',
-                  ];
+    try {
+      switch (category) {
+        case AtServerEvent.atProtocolCategory:
+          final name = ej['name'];
+          switch (name) {
+            case AtServerEvent.atSignPKChangedEventName:
+              logger.shout('HANDLING public key change event: $ej');
+              AtSignPKChangedEvent e = AtSignPKChangedEvent.fromJson(ej);
+              List<String> keysToRemove = [
+                'shared_key.${e.atSign.substring(1)}$atSign',
+                '${e.atSign}:shared_key$atSign',
+                'cached:public:publickey${e.atSign}',
+              ];
 
-                  for (final k in keysToRemove) {
-                    logger.shout('Removing $k from local storage');
-                    await atClient.getLocalSecondary()!.keyStore!.remove(
+              for (final k in keysToRemove) {
+                logger.shout('Removing $k from local storage');
+                await atClient.getLocalSecondary()!.keyStore!.remove(
                       k,
                       skipCommit: true,
                     );
-                  }
-                  logger.shout('HANDLED OK');
-                  break;
-                default:
-                  logger.shout(
-                    'Not handling server event'
-                    ' of category $category'
-                    ' with name $name',
-                  );
-                  break;
               }
+              logger.shout('HANDLED OK');
               break;
             default:
               logger.shout(
                 'Not handling server event'
-                ' of category $category',
+                ' of category $category'
+                ' with name $name',
               );
               break;
           }
-        } catch (e) {
+          break;
+        default:
           logger.shout(
-            'Exception $e while handling server event notification $n',
+            'Not handling server event'
+            ' of category $category',
           );
-        }
-      });
+          break;
+      }
+    } catch (e) {
+      logger.shout(
+        'Exception $e while handling server event notification $n',
+      );
+    }
+  });
 }
