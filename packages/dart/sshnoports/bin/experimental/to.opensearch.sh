bd4c6ed6a12c0bb57e20395b243a80f214665152
gkc <gary.casey@gmail.com>
2025-10-12 22:46:50 +0100

    feat: small tweak to `to.opensearch.sh`

--- packages/dart/sshnoports/bin/experimental/to.opensearch.sh
+++ packages/dart/sshnoports/bin/experimental/to.opensearch.sh
@@ -9,14 +9,16 @@ do
   sessionState=$(jq -r '.state' <<< "$line")
   extendedJson='{"doc":'$line',"doc_as_upsert":true}'
 
-  echo "$sessionState	$sessionId"
+  echo "$sessionId $sessionState"
   curlOutput=$(curl -s -S \
     -X POST "http://localhost:9200/noports/_update/$sessionId" \
     -H "Content-Type: application/json" \
     -d "$extendedJson")
   curlExitCode=${PIPESTATUS[0]}
   if [ "$curlExitCode" != 0 ] ; then
-      echo curl exitCode: "$curlExitCode" output: "$curlOutput"
+    echo '    ' curl exitCode: "$curlExitCode" "$curlOutput"
+  else
+    echo '    ' Uploaded OK
   fi
 done
 
