7e570490bcd77a0b55808a57bc84ab2440a4cd7c
XavierChanth <xchanthavong@gmail.com>
2025-10-29 21:08:06 -0400

    fix: signtool args

--- tools/codesign_windows.sh
+++ tools/codesign_windows.sh
@@ -8,5 +8,5 @@
 # This file should be sourced into GitHub actions
 
 sign_file() {
-  eval "\"$WINDOWS_SIGNTOOL_PATH\" sign $WINDOWS_SIGNTOOL_ARGS \"$1\""
+  eval "signtool.exe sign $WINDOWS_SIGNTOOL_ARGS \"$1\""
 }
