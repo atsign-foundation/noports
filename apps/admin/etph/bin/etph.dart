5af8d27635fccd7e1794c7d8039e7e4750651780
gkc <gary.casey@gmail.com>
2025-12-22 13:02:09 +0000

    feat: etph: tweak

--- apps/admin/etph/bin/etph.dart
+++ apps/admin/etph/bin/etph.dart
@@ -49,11 +49,9 @@ void main(List<String> args) async {
   //     req.params['id'] is int;
   //     req.params['name'] != null;
   //   });
-  app.post('/api/ph/et/:atsign', (req, res) async {
-    String atSign = req.params['atsign'];
-
+  app.post('/api/ph/et', (req, res) async {
     stdout.writeln(
-        '${DateTime.now().toUtc().toIso8601String()} | $atSign | ${await req.body}\n');
+        '${DateTime.now().toUtc().toIso8601String()} | ${await req.body}\n');
 
     res.statusCode = 200;
 
