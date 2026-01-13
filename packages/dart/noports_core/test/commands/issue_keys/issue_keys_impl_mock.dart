4e49c55b0dc06c1df5fff6405deca2a790d67bc9
Srie Teja <srit1505@gmail.com>
2026-01-13 16:31:12 +0530

    test: add unit tests

---
+++ packages/dart/noports_core/test/commands/issue_keys/issue_keys_impl_mock.dart
@@ -0,0 +1,23 @@
+// Extension to allow testing with custom parameters
+import 'package:noports_core/commands.dart';
+
+class IssueKeysTest extends IssueKeys {
+  static const defaultEnrollmentCheckIntervalDuration = Duration(seconds: 1);
+  static const defaultMaxRetries = 1;
+
+  IssueKeysTest.test(
+    super.params, {
+    super.atClient,
+    super.enrollmentService,
+    int? maxRetries,
+    Duration? checkInterval,
+  }) : super(
+         maxRetries: maxRetries ?? defaultMaxRetries,
+         checkInterval: checkInterval ?? defaultEnrollmentCheckIntervalDuration,
+       );
+
+  @override
+  Future<String> generateOTP() async {
+    return '12345A';
+  }
+}
---
+++ packages/dart/noports_core/test/commands/issue_keys/issue_keys_params_test.dart
@@ -0,0 +1,134 @@
+import 'package:at_client/at_client.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:noports_core/commands.dart';
+import 'package:noports_core/src/commands/issue_keys/issue_keys_impl.dart';
+import 'package:noports_core/src/commands/issue_keys/issue_keys_params.dart';
+import 'package:test/test.dart';
+
+import 'issue_keys_impl_mock.dart';
+
+class MockAtClient extends Mock implements AtClient {}
+
+class MockEnrollmentService extends Mock implements EnrollmentService {}
+
+class MockEnrollment extends Mock implements Enrollment {}
+
+void main() {
+  late IssueKeys issueKeys;
+  late MockAtClient mockAtClient;
+  late MockEnrollmentService mockEnrollmentService;
+  late IssueKeysParams params;
+
+  const testAtsign = '@test';
+  const testOtp = '123456';
+  const testDevice = 'test-device';
+
+  setUp(() {
+    mockAtClient = MockAtClient();
+    mockEnrollmentService = MockEnrollmentService();
+    params = IssueKeysParams(
+      atsign: testAtsign,
+      device: testDevice,
+      otp: testOtp,
+    );
+
+    issueKeys = IssueKeysTest.test(
+      params,
+      atClient: mockAtClient,
+      enrollmentService: mockEnrollmentService,
+    );
+  });
+
+  group('IssueKeysParams', () {
+    test('throws for missing args', () {
+      expect(() => IssueKeysParams.fromArgs([]), throwsA(isA<ArgumentError>()));
+    });
+
+    test('throws for missing atsign', () {
+      final args = ['--device', 'my-device'];
+      expect(
+        () => IssueKeysParams.fromArgs(args),
+        throwsA(isA<ArgumentError>()),
+      );
+    });
+
+    test('parses atsign with missing device and passphrase', () {
+      final params = IssueKeysParams.fromArgs(['-a', '@alice']);
+      expect(params.atsign, equals('@alice'));
+      expect(params.device, isNull);
+      expect(params.passPhrase, isNull);
+    });
+
+    test('parses atsign flag with missing device and passphrase', () {
+      final params = IssueKeysParams.fromArgs(['--atsign', '@alice']);
+      expect(params.atsign, equals('@alice'));
+      expect(params.device, isNull);
+      expect(params.passPhrase, isNull);
+    });
+
+    test('parses device name', () {
+      final params = IssueKeysParams.fromArgs([
+        '-a',
+        '@alice',
+        '--device',
+        'my-device',
+      ]);
+      expect(params.device, equals('my-device'));
+    });
+
+    test('parses device name flag', () {
+      final params = IssueKeysParams.fromArgs([
+        '--atsign',
+        '@alice',
+        '-d',
+        'my-device',
+      ]);
+      expect(params.device, equals('my-device'));
+    });
+
+    test('parses key-file', () {
+      final params = IssueKeysParams.fromArgs([
+        '-a',
+        '@alice',
+        '--key-file',
+        'path/to/keyfile',
+      ]);
+      expect(params.atKeysFilePath, equals('path/to/keyfile'));
+    });
+
+    test('parses key-file short flag', () {
+      final params = IssueKeysParams.fromArgs([
+        '-a',
+        '@alice',
+        '-k',
+        'path/to/keyfile',
+      ]);
+      expect(params.atKeysFilePath, equals('path/to/keyfile'));
+    });
+
+    test('parses passphrase', () {
+      final params = IssueKeysParams.fromArgs([
+        '-a',
+        '@alice',
+        '--pass-phrase',
+        'mypass',
+      ]);
+      expect(params.passPhrase, equals('mypass'));
+    });
+
+    test('parses passphrase flag', () {
+      final params = IssueKeysParams.fromArgs(['-a', '@alice', '-P', 'mypass']);
+      expect(params.passPhrase, equals('mypass'));
+    });
+
+    test('parses verbose flag', () {
+      final params = IssueKeysParams.fromArgs(['-a', '@alice', '--verbose']);
+      expect(params.verbose, isTrue);
+    });
+
+    test('parses debug flag', () {
+      final params = IssueKeysParams.fromArgs(['-a', '@alice', '--debug']);
+      expect(params.debug, isTrue);
+    });
+  });
+}
