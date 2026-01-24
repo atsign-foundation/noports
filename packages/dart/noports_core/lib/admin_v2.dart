4ae8edcf2d300d9ad61f6314c02d2daa105ad253
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-01-23 22:44:10 -0500

    chore: move to npp

--- packages/dart/noports_core/lib/admin_v2.dart
+++
@@ -1,9 +0,0 @@
-library noports_core_admin_v2;
-
-export 'src/admin/v2/models.dart';
-export 'src/admin/v2/policy_cache.dart';
-export 'src/admin/v2/policy_client.dart';
-export 'src/admin/v2/policy_data_operation.dart';
-export 'src/admin/v2/policy_service.dart';
-export 'src/admin/v2/policy_cli_params.dart';
-export 'src/admin/v2/policy_data_operation_hooks.dart';
---
+++ packages/dart/noports_core/lib/npp.dart
@@ -0,0 +1,9 @@
+library noports_core_npp;
+
+export 'src/npp/models.dart';
+export 'src/npp/policy_cache.dart';
+export 'src/npp/policy_cli_params.dart';
+export 'src/npp/policy_client.dart';
+export 'src/npp/policy_data_operation.dart';
+export 'src/npp/policy_data_operation_hooks.dart';
+export 'src/npp/policy_service.dart';
