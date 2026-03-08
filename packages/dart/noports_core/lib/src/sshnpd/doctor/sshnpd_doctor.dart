544d59cda6d458cf60bb644f420786d537af3b4c
Lilian <lilian.gardon07@gmail.com>
2026-03-09 00:28:33 +0100

    style: indent the entire `runSshnpdDoctor` function body.

--- packages/dart/noports_core/lib/src/sshnpd/doctor/sshnpd_doctor.dart
+++ packages/dart/noports_core/lib/src/sshnpd/doctor/sshnpd_doctor.dart
@@ -18,30 +18,30 @@ class SshnpdDoctor {
     print('-' * 60);
     print('');
 
-  final runner = DiagnosticRunner(
-    verbose: true,
-    checks: [
-      PrerequisitesCheck(), 
-      ConnectivityCheck(), 
-      KeysCheck(), 
-      ServiceStatusCheck(), 
-      ServiceLogsCheck(), 
-      ConfigCheck(), 
-      VersionCheck(packageVersion), 
-    ],
-  );
-  final results = await runner.runAll();
+    final runner = DiagnosticRunner(
+      verbose: true,
+      checks: [
+        PrerequisitesCheck(), 
+        ConnectivityCheck(), 
+        KeysCheck(), 
+        ServiceStatusCheck(), 
+        ServiceLogsCheck(), 
+        ConfigCheck(), 
+        VersionCheck(packageVersion), 
+      ],
+    );
+    final results = await runner.runAll();
 
-  String summary = runner.generateSummary(results);
+    String summary = runner.generateSummary(results);
 
-  final platform = PlatformUtils.instance;
-  final os = platform.name;
-  final arch = await platform.getArchitecture();
-  final version = VersionCheck(packageVersion).packageVersion;
-  final home = platform.homeDirectory;
-  final keysPath = '$home${Platform.pathSeparator}.atsign${Platform.pathSeparator}keys';
+    final platform = PlatformUtils.instance;
+    final os = platform.name;
+    final arch = await platform.getArchitecture();
+    final version = VersionCheck(packageVersion).packageVersion;
+    final home = platform.homeDirectory;
+    final keysPath =                             '$home${Platform.pathSeparator}.atsign${Platform.pathSeparator}keys';
 
-  final systemInfo = '''
+    final systemInfo = '''
 ${'-' * 60}
 SYSTEM INFORMATION
 ${'-' * 60}
@@ -53,22 +53,22 @@ Keys Path    : $keysPath
 ${'-' * 60}
 ''';
 
-  summary = systemInfo + summary;
+    summary = systemInfo + summary;
 
-  print(summary);
-  try {
-    final results = SshnpdOption.argParser.parse(args);
-    if (results['output'] == true) {
-      String outputName = 'sshnpd_doctor_log.txt';
-      if (results.rest.isNotEmpty) {
-        outputName = results.rest.first;
+    print(summary);
+    try {
+      final results = SshnpdOption.argParser.parse(args);
+      if (results['output'] == true) {
+        String outputName = 'sshnpd_doctor_log.txt';
+        if (results.rest.isNotEmpty) {
+          outputName = results.rest.first;
+        }
+        final file = File(outputName);
+        await file.writeAsString(summary);
+        print('Summary saved to $outputName');
       }
-      final file = File(outputName);
-      await file.writeAsString(summary);
-      print('Summary saved to $outputName');
+    } catch (e) {
+      // Ignore error if parsing fails
     }
-  } catch (e) {
-    // Ignore error if parsing fails
-  }
   }
 }
