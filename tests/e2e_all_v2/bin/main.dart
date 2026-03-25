7f9bd01d460bf01a9370f2327565b9247a39c3bc
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-03-25 16:23:42 -0400

    feat: start on e2e_all_v2/bin/main.dart

---
+++ tests/e2e_all_v2/bin/main.dart
@@ -0,0 +1,10 @@
+import 'package:e2e_all_v2/e2e_all_v2_params.dart';
+
+late AtSignLogg
+
+Future<void> main(List<String> args) async {
+  E2EAllV2Params e2eAllV2Params = E2EAllV2Params.fromArgs(args);
+  logLoadedParams();  
+}
+
+
