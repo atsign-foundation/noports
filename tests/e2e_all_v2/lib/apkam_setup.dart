3b02fd5b9c53ec1c4fc527a40c06be787fccf371
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-04-06 11:33:28 -0400

    fix(test): apkam setup

--- tests/e2e_all_v2/lib/apkam_setup.dart
+++ tests/e2e_all_v2/lib/apkam_setup.dart
@@ -135,7 +135,7 @@ Future<int> enroll({
     enrollArgs,
   );
   print('Waiting for enrollment approval for ${atsign}...');
-  sleep(const Duration(seconds: 2));
+  sleep(const Duration(seconds: 5));
 
   // approve enrollment
   print('Approving enrollment request for $atsign with apkamApp $apkamApp and apkamDeviceName $apkamDeviceName');
