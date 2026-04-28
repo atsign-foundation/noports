193d1bbd92deed27034cf9a6d1bc2bd190fcc6ec
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-04-27 20:22:06 -0400

    style: core_tests_utils.dart

--- tests/e2e_all_v2/lib/core_tests/core_tests_utils.dart
+++ tests/e2e_all_v2/lib/core_tests/core_tests_utils.dart
@@ -5,7 +5,7 @@ import 'package:e2e_all_v2/noports_version.dart';
 // '5.9.4' --> '594', '0.0.1' --> '001'
 String _versionForDeviceName(final String version) {
   String s;
-  if(version == 'current') {
+  if (version == 'current') {
     s = 'c';
     return s;
   }
@@ -14,7 +14,7 @@ String _versionForDeviceName(final String version) {
   s = version.replaceAll('.', '').replaceAll('v', '').replaceAll('c', '');
 
   // maximum 6 characters of s
-  if(s.length > 6) {
+  if (s.length > 6) {
     s = s.substring(0, 6);
   }
   return s;
