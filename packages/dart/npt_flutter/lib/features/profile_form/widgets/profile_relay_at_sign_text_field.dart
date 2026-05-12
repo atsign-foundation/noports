f0905ebe3bd5af77d3079eb0eb3db7148cc30130
Curtly Critchlow <curtlycritchlow@ccwc.io>
2026-05-12 09:09:54 -0400

    fix: update import statement to use at_client_flutter instead of at_onboarding_flutter.

--- packages/dart/npt_flutter/lib/features/profile_form/widgets/profile_relay_at_sign_text_field.dart
+++ packages/dart/npt_flutter/lib/features/profile_form/widgets/profile_relay_at_sign_text_field.dart
@@ -1,4 +1,4 @@
-import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
+import 'package:at_client_flutter/at_client_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:npt_flutter/features/profile/profile.dart';
