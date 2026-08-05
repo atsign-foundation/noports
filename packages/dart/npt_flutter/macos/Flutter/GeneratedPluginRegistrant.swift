aa9426776c4cabfd31ebb19871c2fbffb9cf2af8
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-05 11:50:31 -0400

    fix:

--- packages/dart/npt_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/dart/npt_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import device_info_plus
 import file_picker
 import package_info_plus
 import screen_retriever_macos
-import shared_preferences_foundation
 import tray_manager
 import url_launcher_macos
 import window_manager
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
