7e4b3cb8848a4b4fbb83ac96b829d3c5cb2e9867
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-05 21:26:19 -0400

    fix(ios/linux/macos/windows): files

--- packages/dart/npt_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/dart/npt_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,32 +5,26 @@
 import FlutterMacOS
 import Foundation
 
-import at_file_saver
 import biometric_storage
 import desktop_drop
 import device_info_plus
 import file_picker
 import package_info_plus
 import screen_retriever_macos
-import share_plus
 import shared_preferences_foundation
 import tray_manager
 import url_launcher_macos
-import webview_flutter_wkwebview
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
