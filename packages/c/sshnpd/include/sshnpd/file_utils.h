58bdf97c090757cac6f9654a9d21461263d937f5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-22 10:00:42 -0400

    test(csshnpd): unit test gaps

--- packages/c/sshnpd/include/sshnpd/file_utils.h
+++ packages/c/sshnpd/include/sshnpd/file_utils.h
@@ -1,6 +1,7 @@
 #ifndef SSH_KEY_UTIL_H
 #define SSH_KEY_UTIL_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 enum supported_key_prefix {
@@ -20,5 +21,6 @@ typedef struct {
   char *key;
 } authkeys_params;
 
+bool is_valid_ssh_public_key(const char *ssh_key);
 int authorize_ssh_public_key(authkeys_params *params);
 #endif
