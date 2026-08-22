fdbcbed7789fc69b87f07689b478dae47ab966c4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-21 23:45:32 -0400

    refactor(csshnpd): individual switch statements instead of ifs

--- packages/c/sshnpd/src/daemon.c
+++ packages/c/sshnpd/src/daemon.c
@@ -203,45 +203,60 @@ void main_loop() {
         }
 
         switch (notification_key) {
-        case NK_SSHPUBLICKEY:
-        case NK_SSH_REQUEST:
-        case NK_NPT_REQUEST:
         case NK_PING: {
           if (!is_manager_atsign(&params, message.notification->from)) {
             atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_ERROR,
                          "Rejecting request from unauthorized atSign: %s\n", message.notification->from);
             break;
           }
-
-          if (notification_key == NK_PING) {
-            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_ping\n");
-            handle_ping(&params, &message, ping_response, &worker);
-          } else if (notification_key == NK_SSHPUBLICKEY) {
-            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_sshpublickey\n");
-            handle_sshpublickey(&params, &message, authkeys_file, authkeys_filename);
-          } else if (notification_key == NK_SSH_REQUEST) {
-            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_ssh_request\n");
-            permitopen.requested_host = "localhost";
-            permitopen.requested_port = params.local_sshd_port;
-            if (!should_permitopen(&permitopen)) {
-              atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Ignoring request to localhost:%d\n",
-                           params.local_sshd_port);
-              break;
-            }
-            handle_ssh_request(&worker, &params, &is_child_process, &message, signingkey);
-            if (is_child_process) {
-              atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Exiting child process\n");
-              atclient_monitor_message_free(&message);
-              return;
-            }
-          } else {
-            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_npt_request\n");
-            handle_npt_request(&worker, &params, &is_child_process, &message, signingkey);
-            if (is_child_process) {
-              atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Exiting child process\n");
-              atclient_monitor_message_free(&message);
-              return;
-            }
+          atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_ping\n");
+          handle_ping(&params, &message, ping_response, &worker);
+          break;
+        }
+        case NK_SSHPUBLICKEY: {
+          if (!is_manager_atsign(&params, message.notification->from)) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_ERROR,
+                         "Rejecting request from unauthorized atSign: %s\n", message.notification->from);
+            break;
+          }
+          atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_sshpublickey\n");
+          handle_sshpublickey(&params, &message, authkeys_file, authkeys_filename);
+          break;
+        }
+        case NK_SSH_REQUEST: {
+          if (!is_manager_atsign(&params, message.notification->from)) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_ERROR,
+                         "Rejecting request from unauthorized atSign: %s\n", message.notification->from);
+            break;
+          }
+          atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_ssh_request\n");
+          permitopen.requested_host = "localhost";
+          permitopen.requested_port = params.local_sshd_port;
+          if (!should_permitopen(&permitopen)) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Ignoring request to localhost:%d\n",
+                         params.local_sshd_port);
+            break;
+          }
+          handle_ssh_request(&worker, &params, &is_child_process, &message, signingkey);
+          if (is_child_process) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Exiting child process\n");
+            atclient_monitor_message_free(&message);
+            return;
+          }
+          break;
+        }
+        case NK_NPT_REQUEST: {
+          if (!is_manager_atsign(&params, message.notification->from)) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_ERROR,
+                         "Rejecting request from unauthorized atSign: %s\n", message.notification->from);
+            break;
+          }
+          atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Executing handle_npt_request\n");
+          handle_npt_request(&worker, &params, &is_child_process, &message, signingkey);
+          if (is_child_process) {
+            atlogger_log(LOGGER_TAG, ATLOGGER_LOGGING_LEVEL_DEBUG, "Exiting child process\n");
+            atclient_monitor_message_free(&message);
+            return;
           }
           break;
         }
